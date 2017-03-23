6f4c54ac93dddde69ebd57d9438bf40a47c8840a
Hovnatan Karapetyan <hovnatan@gmail.com>
2017-03-23 21:37:34 +0400

    Add mpv settings

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 cd ~
+
+rm -rf ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux.conf .tmux.conf
+
+rm -rf ~/.config/redshift.conf
 ln -s ~/.dotfiles/redshift.conf ~/.config/redshift.conf
+
+rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
 mkdir -p ~/.vimundo/
+
+rm -rf ~/.config/fish
 ln -s ~/.dotfiles/fish/ ~/.config/fish
+
+rm -rf ~/.config/mpv
+ln -s ~/.dotfiles/mpv/ ~/.config/mpv
