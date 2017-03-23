90060fff4072acc20c7ae6075b27d19594757a03
Hovnatan Karapetyan <hovnatan@gmail.com>
2017-03-23 21:33:49 +0400

    Create vimundo directory

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ cd ~
 ln -s ~/.dotfiles/.tmux.conf .tmux.conf
 ln -s ~/.dotfiles/redshift.conf ~/.config/redshift.conf
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
+mkdir -p ~/.vimundo/
 ln -s ~/.dotfiles/fish/ ~/.config/fish
