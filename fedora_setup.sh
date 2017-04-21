cc378c3566b2c5361ae21b85e9dac4deee687afc
Hovnatan Karapetyan <hovnatan@gmail.com>
2017-04-21 15:09:25 +0400

    Disable automatic updates and add sudo to commands

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 
-dnf install calibre htop nautilus-open-terminal transmission python3-devel git-cola mpv qt-creator gnome-tweak-tool fuse fuse-devel libcurl-devel automake go
+sudo dnf install calibre htop nautilus-open-terminal transmission python3-devel git-cola mpv qt-creator gnome-tweak-tool fuse fuse-devel libcurl-devel automake go
 
 # video acceleration
-dnf install vdpauinfo libva-vdpau-driver libva-utils
+sudo dnf install vdpauinfo libva-vdpau-driver libva-utils
 
 # gstream plugins
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf install gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0
-dnf install gstreamer{1,}-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0
+sudo dnf install gstreamer{1,}-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0
 
 # nvim
-dnf install nvim
+sudo dnf install nvim
 
 # tmux 
-dnf install tmux xclip 
+sudo dnf install tmux xclip 
 
 # fish
-dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_25/shells:fish:release:2.repo
-dnf install fish
+sudo dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_25/shells:fish:release:2.repo
+sudo dnf install fish
 
 # python spyder
-dnf install python3-pip 
-pip3 install spyder
+sudo dnf install python3-pip 
+sudo pip3 install spyder
 
 # install recoll 
-dnf install recoll djvulibre antiword unzip libxslt-python
-dnf install python-pip
-pip2 install epub 
+sudo dnf install recoll djvulibre antiword unzip libxslt-python
+sudo dnf install python-pip
+sudo pip2 install epub 
 
 # gnome setup
 gsettings set org.gnome.desktop.input-sources xkb-options  "['caps:ctrl_modifier', 'grp:lalt_lshift_toggle', 'grp:switch']"
 gsettings set org.gnome.desktop.input-sources mru-sources "[('xkb', 'us'), ('xkb', 'ru+phonetic_winkeys'), ('xkb', 'am+phonetic-alt')]"
 gsettings set org.gnome.desktop.input-sources per-window "true"
+gsettings set org.gnome.software download-updates false
+sudo systemctl disable dnf-makecache.timer
+sudo systemctl disable dnf-makecache.service
 
 # rc.local for trackpoint adjustment
-cp -i rc.local /etc/rc.d/
+sudo cp -i rc.local /etc/rc.d/
 
 echo "Manually install goofys (s3 fs)"
 
