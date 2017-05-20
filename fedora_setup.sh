c8cf7acff37ea607be2a6620b4bd653983fad0df
xx <xx>
2017-05-20 13:30:42 +0400

    dnf install default yes

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-sudo dnf install calibre htop nautilus-open-terminal transmission python3-devel git-cola mpv qt-creator gnome-tweak-tool fuse fuse-devel libcurl-devel automake go texinfo redhat-rpm-config vdpauinfo libva-vdpau-driver libva-utils neovim tmux xclip python3-pip recoll djvulibre antiword unzip libxslt-python python-pip fortune-mod workrave redshift-gtk
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=8000000
+sudo chmod 0644 /swapfile
+sudo mkswap /swapfile
+sudo swapon /newswap
+sudo echo "/swapfile    swap    swap   defaults 0 0" >> /etc/fstab
+
+sudo dnf -y upgrade --refresh
+
+sudo dnf -y install calibre htop nautilus-open-terminal transmission python3-devel git-cola mpv qt-creator gnome-tweak-tool fuse fuse-devel libcurl-devel automake go texinfo redhat-rpm-config vdpauinfo libva-vdpau-driver libva-utils neovim tmux xclip python3-pip recoll djvulibre antiword unzip libxslt-python python-pip fortune-mod workrave redshift-gtk
 
 # non free packages
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} gstreamer{1,}-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs freetype-freeworld dropbox --setopt=strict=0
 
 # fish
-sudo dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_$(rpm -E %fedora)/shells:fish:release:2.repo
-sudo dnf install fish
+sudo dnf -y config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_$(rpm -E %fedora)/shells:fish:release:2.repo
+sudo dnf -y install fish
 
-# python spyder  
+# python science libs  
 sudo pip3 install --upgrade numpy scipy pandas statsmodels scikit-learn matplotlib notebook pyqt5 spyder
 
-# install recoll 
+# install for recoll 
 sudo pip2 install epub 
 
 # gnome setup
