bf3e7b7ff56d4a3c00df67400c41b7cc646b8a15
Hovnatan Karapetyan <hovnatan@gmail.com>
2017-06-04 22:49:09 +0400

    use vim-plug

--- setup.sh
+++ setup.sh
@@ -13,10 +13,8 @@ ln -s ~/.dotfiles/redshift.conf ~/.config/redshift.conf
 rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
 mkdir -p ~/.vimundo/
-mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle && \
-curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.config/nvim
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 rm -rf ~/.config/fish
 ln -s ~/.dotfiles/fish/ ~/.config/fish
