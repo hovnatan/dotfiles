1c2834d20417acec02deea47846e7cdbf2755340
Hovnatan Karapetyan <hovnatan@gmail.com>
2018-01-30 14:24:37 +0400

    fish shell to vim mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,20 @@
-function fish_user_key_bindings 
+function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
     bind \cp up-or-search
     bind \cn down-or-search
     bind \ep history-token-search-backward
     bind \en history-token-search-forward
     fzf_key_bindings
 end
+
+set -g fish_key_bindings hybrid_bindings
+
+set -U fish_cursor_default block
+set -U fish_cursor_insert block blink
+set -U fish_cursor_visual block
 
 set -u fish_color_cwd cyan
 set -u fish_color_command normal
