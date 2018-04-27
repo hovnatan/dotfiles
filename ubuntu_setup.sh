c7111091729e3a7dea0b6a08cb2c9a259e24cde8
Hovnatan Karapetyan <hovnatan@gmail.com>
2018-04-27 09:26:04 +0400

    remove tracker

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+tracker daemon -t
+cd ~/.config/autostart
+cp -v /etc/xdg/autostart/tracker-* ./
+for FILE in `ls`; do echo Hidden=true >> $FILE; done
+rm -rf ~/.cache/tracker ~/.local/share/tracker
+
+sudo apt update; sudo apt -y dist-upgrade
+
 sudo apt -y install spyder3 git-cola net-tools qtcreator chrome-gnome-shell lm-sensors tmux fish python3-gdal python-gdal neovim libfreetype6-dev gfortran libopenblas-dev liblapack-dev tk-dev cmake qtdeclarative5-dev libtbb-dev libeigen3-dev libvtk6-dev zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libopenexr-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev liblapack-dev liblapacke-dev checkinstall python3-numpy python-numpy libblas-dev gfortran libatlas-base-dev libboost-all-dev ccache fonts-hack-ttf build-essential cmake pkg-config unzip workrave redshift-gtk clang-tools clang-format libgdal-dev valgrind automake python3-pip python-pip neovim curl fortune-mod fortunes htop uget vainfo mpv jupyter-notebook jupyter-core python-ipykernel python3-pandas awscli
 
 sudo apt -y install evolution gnome-session 
@@ -47,5 +57,3 @@ git remote set-url origin git@github.com:hovnatan/dotfiles.git
 # cp ~/.dotfiles/70-pointingstick.hwdb /lib/udev/hwdb.d/70-pointingstick.hwdb 
 # sudo udevadm hwdb -u
 # sudo udevadm trigger
-
-
