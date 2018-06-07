8035ab9666d0c5294042a7beea5f6c6655350b83
xx <xx>
2018-06-07 13:13:40 +0400

    Add feh config

--- setup.sh
+++ setup.sh
@@ -11,18 +11,18 @@ rm -rf ~/.config/redshift.conf
 ln -s ~/.dotfiles/.config/redshift.conf ~/.config/redshift.conf
 
 rm -rf ~/.config/nvim
-ln -s ~/.dotfiles/.config/nvim/ ~/.config/nvim
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 mkdir -p ~/.vimundo/
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c 'PlugInstall | qa'
 
 rm -rf ~/.config/fish
-ln -s ~/.dotfiles/.config/fish/ ~/.config/fish
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
 chsh -s `which fish`
 
 rm -rf ~/.config/mpv
-ln -s ~/.dotfiles/.config/mpv/ ~/.config/mpv
+ln -s ~/.dotfiles/.config/mpv ~/.config/mpv
 
 rm -rf ~/.gitconfig
 cp ~/.dotfiles/.gitconfig ~/
@@ -34,3 +34,6 @@ ln -s ~/.dotfiles/workrave.ini ~/.workrave/workrave.ini
 rm -rf ~/.config/htop
 mkdir ~/.config/htop
 ln -s ~/.dotfiles/.config/htoprc ~/.config/htop/htoprc
+
+rm -rf ~/.config/feh
+ln -s ~/.dotfiles/.config/feh ~/.config/feh
