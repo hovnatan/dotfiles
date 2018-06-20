f146811acb707a80b46d0dd95fa27b5a1dec4a6e
Hovnatan Karapetyan <hovnatan@gmail.com>
2018-06-20 11:06:14 +0400

    feh improve

--- scripts/feh.sh
+++ scripts/feh.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-
 last_arg="${@: -1}"
+#echo $last_arg
 
 extension="${last_arg##*.}"
 #echo $extension
-dir1=$(dirname "${last_arg}")
+dir=$(dirname "${last_arg}")
+filename=$(basename "${last_arg}")
 #echo $dir1
+#echo $filename1
 
 length=$(($#-1))
 array=${@:1:$length}
 #echo $array
-feh $array --start-at $last_arg $dir1/*.$extension 2>/dev/null
+feh $array --start-at $dir/$filename $dir/*.$extension 2>/dev/null
