19cf1ec612e8cf96caab2b4fd5fc33d031592359
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-02-28 22:12:05 +0400

    Better arch setup

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S networkmanager os-prober openssh 
+sudo pacman -Sy networkmanager os-prober openssh gnome-weather pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave gnome-tweaks base-devel chrome-gnome-shell feh mpv xclip neovim fzf tmux youtube-dl gnome fish intel-ucode grub 
+
+ 
