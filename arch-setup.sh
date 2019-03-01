52f62c336bb8c0e2bc4f1c669a694a6318481bac
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-01 09:48:33 +0400

    Firmware installation

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Sy networkmanager os-prober openssh gnome-weather pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave gnome-tweaks base-devel chrome-gnome-shell feh mpv xclip neovim fzf tmux youtube-dl gnome fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 gdm
+pacman -Sy networkmanager os-prober openssh gnome-weather pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave gnome-tweaks base-devel chrome-gnome-shell feh mpv xclip neovim fzf tmux youtube-dl gnome fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 gdm sudo
 
 sudo systemctl enable tlp.service
 sudo systemctl enable gdm.service
@@ -12,4 +12,6 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S skype git-cola ttf-ms-fonts dropbox 
+yay -S skype git-cola ttf-ms-fonts dropbox wd719x-firmware aic94xx-firmware
+sudo mkinitcpio -p linux
+
