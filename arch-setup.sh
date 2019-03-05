1cb6022e6b06a5572a410fda54e747265656c696
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-05 09:50:54 +0400

    redshift

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Sy networkmanager os-prober openssh gnome-weather pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave gnome-tweaks base-devel chrome-gnome-shell feh mpv xclip neovim fzf tmux youtube-dl gnome fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 gdm sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf
+pacman -Sy networkmanager os-prober openssh gnome-weather pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave gnome-tweaks base-devel chrome-gnome-shell feh mpv xclip neovim fzf tmux youtube-dl gnome fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 gdm sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift
 
 sudo systemctl enable tlp.service
 sudo systemctl enable lightdm.service
