40776597ea2db6efad9e89b0454e905db918a35f
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-06 15:54:15 +0400

    dunst

--- arch-setup.sh
+++ arch-setup.sh
@@ -2,7 +2,15 @@
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -Sy networkmanager os-prober openssh pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave base-devel feh mpv xclip neovim fzf tmux youtube-dl fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift chromium i3-wm i3status i3lock qt5ct xrandr python-gobject aws-cli unzip xautolock imagemagick scrot pulseaudio-alsa pulsemixer pulsemixer fuseiso
+pacman -Sy networkmanager os-prober openssh pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave base-devel feh mpv xclip neovim fzf tmux youtube-dl fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift chromium i3-wm i3status i3lock qt5ct xrandr python-gobject aws-cli unzip xautolock imagemagick scrot pulseaudio-alsa pulsemixer pulsemixer fuseiso ttf-liberation noto-fonts
+
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+#Uncomment  export FREETYPE_PROPERTIES="truetype:interpreter-version=40"
+sudo nvim  /etc/profile.d/freetype2.sh
+# to install dunst as notifier edit to /usr/bin/dunst 
+#sudo nvim /usr/share/dbus-1/services/org.freedesktop.Notifications.service
 
 #allow_others in /etc/fuse.conf
 #pacman -Sy gnome gnome-weather gnome-tweaks chrome-gnome-shell gdm
