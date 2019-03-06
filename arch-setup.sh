531134dcac0abe7270c3ee8b7bee9baa0b799dc5
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-06 14:21:14 +0400

    xautolock

--- arch-setup.sh
+++ arch-setup.sh
@@ -2,7 +2,7 @@
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -Sy networkmanager os-prober openssh pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave base-devel feh mpv xclip neovim fzf tmux youtube-dl fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift chromium i3-wm i3status i3lock qt5ct xrandr python-gobject aws-cli unzip
+pacman -Sy networkmanager os-prober openssh pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave base-devel feh mpv xclip neovim fzf tmux youtube-dl fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift chromium i3-wm i3status i3lock qt5ct xrandr python-gobject aws-cli unzip xautolock imagemagick scrot
 
 #pacman -Sy gnome gnome-weather gnome-tweaks chrome-gnome-shell gdm
 
--- lock.sh
+++ lock.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-pgrep i3lock || i3lock -c 000000
+if pgrep i3lock 
+then
+else
+# Take a screenshot
+scrot /tmp/screen_locked.png
+
+# Pixellate it 10x
+mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -i /tmp/screen_locked.png
+
+# Turn the screen off after a delay.
+sleep 60; pgrep i3lock && xset dpms force off
+fi
