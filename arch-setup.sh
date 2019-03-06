f00668bd0fb08ce1462969dbcf0375897c47b40f
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-06 14:52:35 +0400

    Pulse audio

--- arch-setup.sh
+++ arch-setup.sh
@@ -2,13 +2,14 @@
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -Sy networkmanager os-prober openssh pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave base-devel feh mpv xclip neovim fzf tmux youtube-dl fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift chromium i3-wm i3status i3lock qt5ct xrandr python-gobject aws-cli unzip xautolock imagemagick scrot
+pacman -Sy networkmanager os-prober openssh pepper-flash qtcreator spyder3 python-numpy ipython opencv gdal python-gdal python-opencv ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp htop intel-gpu-tools cmake libva-intel-driver libva-utils intel-media-driver ttf-ubuntu-font-family ttf-hack ttf-dejavu xorg fortune-mod workrave base-devel feh mpv xclip neovim fzf tmux youtube-dl fish intel-ucode grub python-matplotlib glew vtk ccache boost eigen3 sudo xf86-video-intel davfs2 rofi dunst vifm termite python-matplotlib lightdm lightdm-gtk-greeter zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift chromium i3-wm i3status i3lock qt5ct xrandr python-gobject aws-cli unzip xautolock imagemagick scrot pulseaudio-alsa pulsemixer pulsemixer
 
 #pacman -Sy gnome gnome-weather gnome-tweaks chrome-gnome-shell gdm
 
 sudo systemctl enable tlp.service
 sudo systemctl enable lightdm.service
 sudo systemctl enable NetworkManager.service
+systemctl --user enable pulseaudio
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -19,4 +20,3 @@ git clone https://github.com/hastinbe/i3-volume.git
 
 yay -S skype git-cola ttf-ms-fonts dropbox wd719x-firmware aic94xx-firmware zoom
 sudo mkinitcpio -p linux
-
