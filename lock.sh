301c94624f8094ffec571ffe0eaabf6404ac0d2f
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-18 16:35:18 +0400

    pixelate at 5 percent

--- lock.sh
+++ lock.sh
@@ -4,8 +4,8 @@ then
   # Take a screenshot
   scrot /tmp/screen_locked.png
 
-  # Pixellate it 10x
-  mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+  # Pixellate it 5x
+  mogrify -scale 5% -scale 2000% /tmp/screen_locked.png
 
   # Lock screen displaying this image.
   i3lock -i /tmp/screen_locked.png
