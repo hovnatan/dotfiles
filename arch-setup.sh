eea9ed16a935d5464400b53f650fece6d055308e
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-18 12:35:40 +0400

    clipster

--- arch-setup.sh
+++ arch-setup.sh
@@ -39,5 +39,5 @@ makepkg -si
 cd ~
 git clone https://github.com/hastinbe/i3-volume.git
 
-yay -S skype git-cola dropbox wd719x-firmware aic94xx-firmware zoom xkblayout-state-git i3ipc-python libinput-gestures
+yay -S skype git-cola dropbox wd719x-firmware aic94xx-firmware zoom xkblayout-state-git i3ipc-python libinput-gestures clipster
 sudo mkinitcpio -p linux
