6a387cc82f89ae4d250b66b64a19778e75778e8e
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-21 18:51:30 +0400

    typo fix

--- arch-setup.sh
+++ arch-setup.sh
@@ -39,5 +39,7 @@ makepkg -si
 cd ~
 git clone https://github.com/hastinbe/i3-volume.git
 
+# change tar.gz to tar
+sudo nvim /etc/makepkg.conf
 yay -S skype git-cola dropbox wd719x-firmware aic94xx-firmware zoom xkblayout-state-git i3ipc-python libinput-gestures clipster xcwd-git
 sudo mkinitcpio -p linux
