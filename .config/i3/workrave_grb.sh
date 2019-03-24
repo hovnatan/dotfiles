220291865a5967d3db906199771b075b8e274501
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-24 12:07:34 +0400

    wrkrave

---
+++ .config/i3/workrave_grb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+timer=$(dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.ConfigInterface.GetInt string:'timers/rest_break/limit' | grep int32 | awk '{print $2}')
+
+elapsed=$(dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.GetTimerElapsed string:'restbreak' | grep int32 | awk '{print $2}')
+
+seconds_left=`expr $timer - $elapsed`
+
+minutes=$((seconds_left / 60))
+seconds=$((seconds_left % 60))
+
+notify-send -t 3000 "${minutes}:${seconds} left"
+
