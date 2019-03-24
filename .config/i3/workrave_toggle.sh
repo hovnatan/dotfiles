0a1cc0b593781c69527f6fe376f54efd2e2d8ba2
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-03-24 09:45:36 +0400

    workrave dbus

---
+++ .config/i3/workrave_toggle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+new_mode="normal"
+dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.GetOperationMode | grep normal
+if [ $? -eq 0 ]; then
+  new_mode="quiet"
+fi
+
+dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.SetOperationMode string:"${new_mode}"
