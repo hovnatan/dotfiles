354701c01b3fb3d986a5357e723b0b597cf916f8
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-04-17 11:48:05 +0400

    edit

--- arch-setup.sh
+++ arch-setup.sh
@@ -26,7 +26,7 @@ sudo nvim /etc/makepkg.conf
 #for dropbox
 wget https://linux.dropbox.com/fedora/rpm-public-key.asc
 gpg --import rpm-public-key.asc
-yay -S skype git-cola dropbox wd719x-firmware aic94xx-firmware zoom xkblayout-state-git i3ipc-python libinput-gestures clipster xcwd-git foxitreader i3lock-color gruvbox-icon-theme pycharm-professional
+yay -S skype git-cola dropbox wd719x-firmware aic94xx-firmware zoom xkblayout-state-git i3ipc-python libinput-gestures clipster xcwd-git foxitreader i3lock-color gruvbox-icon-theme pycharm-professional python-torchvision
 sudo mkinitcpio -p linux
 
 #yay -S interception-caps2esc
