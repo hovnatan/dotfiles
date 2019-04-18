7e1aa91d8b065c5623011d5dd5bb3e87312246bc
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-04-18 11:01:58 +0400

    tmux full

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,6 +36,6 @@ end
 set -U fish_user_paths ~/.dotfiles/bin
 
 if test -d ~/anaconda3/bin
-  set -U fish_user_paths ~/anaconda3/bin
+  set -U fish_user_paths ~/anaconda3/bin $fish_user_paths
   source (conda info --root)/etc/fish/conf.d/conda.fish
 end
