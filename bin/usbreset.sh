a0b11f43f421dc3b5b7c032a35ee0ab8c824bad1
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-04-19 10:57:48 +0400

    xhci

--- bin/usbreset.sh
+++ bin/usbreset.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 sudo rmmod ehci_pci
+#sudo rmmod xhci_pci
+
 sudo modprobe ehci_pci
+#sudo modprobe xhci_pci
+
 setxkbmap -layout "us,am" -variant ",phonetic-alt" -option "ctrl:nocaps"
