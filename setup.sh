d5d0bf0e81c21cc01b98c2b291a61c2347a52f77
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-04-28 17:28:01 +0400

    coc plugins

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ mkdir -p ~/.vimundo/
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c 'PlugInstall | qa'
+nvim -c 'CocInstall coc-python coc-json coc-word'
 
 rm -rf ~/.config/fish
 ln -s ~/.dotfiles/.config/fish ~/.config/fish
