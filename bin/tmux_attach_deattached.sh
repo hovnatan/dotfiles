e4a2914431381f72b145a46e6c8db3a724809585
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-05-06 12:04:19 +0400

    bug fix to consider only first line

--- bin/tmux_attach_deattached.sh
+++ bin/tmux_attach_deattached.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-session=$(tmux list-sessions | grep -v attached | sed -rn 's/^([0-9]+):.*/\1/p')
+session=$(tmux list-sessions | grep -v attached | head -n 1 | sed -rn 's/^([0-9]+):.*/\1/p')
 if [ -z $session ] ; then
   tmux
 else
