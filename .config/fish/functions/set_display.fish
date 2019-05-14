0e342c234117fdc1f3b48c7f97be9f1de2d862fd
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-05-14 18:07:25 +0400

    Cleanup

--- .config/fish/functions/set_display.fish
+++ .config/fish/functions/set_display.fish
@@ -1,3 +1,3 @@
 function set_display
-    set --export DISPLAY (tmux show-environment DISPLAY | sed -rn 's/^DISPLAY=(.*).*/\1/p')
+    set --export DISPLAY (tmux show-environment DISPLAY | sed -rn 's/^DISPLAY=(.*)/\1/p')
 end
