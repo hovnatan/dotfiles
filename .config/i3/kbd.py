e05dc8cd06cd6e9b45a56e4d9b7c69806ace1039
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-05-30 17:37:15 +0400

    pandoc-citeproc

--- .config/i3/kbd.py
+++ .config/i3/kbd.py
@@ -4,11 +4,13 @@ import subprocess
 import i3ipc
 import signal
 
+
 def handle_pdb(sig, frame):
     import pdb
     pdb.Pdb().set_trace(frame)
 
-DEBUG = False
+
+DEBUG = True
 
 i3 = i3ipc.Connection()
 
@@ -16,47 +18,59 @@ lays = {}
 previous_focus = -1
 deleted_focus = -1
 
+
 def on_window_focus(i3, e):
+    if DEBUG:
+        f.write(f"Len {len(lays)}\n".encode())
     global previous_focus
     if previous_focus != -1 and previous_focus != deleted_focus:
         if DEBUG:
-            print("Previous focus found.")
+            f.write("Previous focus found.\n".encode())
         result = subprocess.run(['check_layout.sh'], stdout=subprocess.PIPE)
         if DEBUG:
-            print(f"Current layout {result}.")
+            f.write(f"Current layout {result}.\n".encode())
         if result.stdout.decode() == "am":
             lays[previous_focus] = 1
         else:
             lays[previous_focus] = 0
     else:
         if DEBUG:
-            print("Previous focus not found.")
+            f.write("Previous focus not found.\n".encode())
     current_focus = i3.get_tree().find_focused().id
     subprocess.run(["setxkbmap", "-option", ""])
     if current_focus not in lays:
         if DEBUG:
-            print(f"{current_focus} not found setting to us.")
-        subprocess.run(["setxkbmap", "-layout", "us,am",
-                        "-variant", ",phonetic-alt",
-                        "-option", "ctrl:nocaps,grp:rctrl_switch"])
+            f.write(f"{current_focus} not found setting to us.\n".encode())
+        subprocess.run(
+            [
+                "setxkbmap", "-layout", "us,am", "-variant", ",phonetic-alt",
+                "-option", "ctrl:nocaps,grp:rctrl_switch"
+            ]
+        )
         lays[current_focus] = 0
         # print(current_focus)
     else:
         if DEBUG:
-            print(f"{current_focus} found.")
+            f.write(f"{current_focus} found.\n".encode())
         val = lays[current_focus]
         if val == 0:
-            subprocess.run(["setxkbmap", "-layout", "us,am",
-                            "-variant", ",phonetic-alt",
-                            "-option", "ctrl:nocaps,grp:rctrl_switch"])
+            subprocess.run(
+                [
+                    "setxkbmap", "-layout", "us,am", "-variant",
+                    ",phonetic-alt", "-option", "ctrl:nocaps,grp:rctrl_switch"
+                ]
+            )
             if DEBUG:
-                print(f"{current_focus} setting to us.")
+                f.write(f"{current_focus} setting to us.\n".encode())
         else:
-            subprocess.run(["setxkbmap", "-layout", "am,us",
-                            "-variant", "phonetic-alt,",
-                            "-option", "ctrl:nocaps,grp:rctrl_switch"])
+            subprocess.run(
+                [
+                    "setxkbmap", "-layout", "am,us", "-variant",
+                    "phonetic-alt,", "-option", "ctrl:nocaps,grp:rctrl_switch"
+                ]
+            )
             if DEBUG:
-                print(f"{current_focus} setting to am")
+                f.write(f"{current_focus} setting to am\n".encode())
     subprocess.run(["pkill", "-RTMIN+10", "i3blocks"])
     previous_focus = current_focus
 
@@ -69,6 +83,9 @@ def on_window_close(i3, e):
         deleted_focus = current_focus
 
 
+if DEBUG:
+    buffsize = 0
+    f = open("/tmp/kbd_py.log", "wb", buffering=buffsize)
 i3.on("window::focus", on_window_focus)
 i3.on("window::close", on_window_close)
 signal.signal(signal.SIGUSR1, handle_pdb)
