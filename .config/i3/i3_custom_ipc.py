f2e6059cc972abf15f93d8e352da11adffd7c066
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-06-10 16:12:36 +0400

    add coc list

--- .config/i3/i3_custom_ipc.py
+++ .config/i3/i3_custom_ipc.py
@@ -1,64 +1,83 @@
 #!/usr/bin/env python3
 
+import os
+import socket
+import selectors
 import subprocess
+import threading
+from argparse import ArgumentParser
+import collections
+
 import i3ipc
-import signal
-from threading import Thread, Lock
 
-mutex = Lock()
-
-
-def handle_pdb(sig, frame):
-    import pdb
-    pdb.Pdb().set_trace(frame)
+SOCKET_FILE = '/tmp/i3_focus_last'
+MAX_WIN_HISTORY = 15
 
 
-DEBUG = False
+class FocusWatcher:
+    def __init__(self):
+        self.i3 = i3ipc.Connection()
+        self.i3.on('window::focus', self.on_window_focus)
+        self.i3.on("window::close", self.on_window_close)
+        self.i3.on("workspace::focus", self.on_workspace_focus)
+        self.listening_socket = socket.socket(
+            socket.AF_UNIX, socket.SOCK_STREAM
+        )
+        if os.path.exists(SOCKET_FILE):
+            os.remove(SOCKET_FILE)
+        self.listening_socket.bind(SOCKET_FILE)
+        self.listening_socket.listen(1)
+        self.window_list = collections.OrderedDict()
+        self.window_list_lock = threading.RLock()
 
-i3 = i3ipc.Connection()
+    def on_window_close(self, i3conn, event):
+        with self.window_list_lock:
+            window_id = event.container.id
+            if window_id in self.window_list:
+                del self.window_list[window_id]
 
-lays = {}
-previous_focus = -1
-deleted_focus = -1
+    def on_workspace_focus(self, i3conn, event):
+        subprocess.run(
+            [
+                "dunstify", "-t", "2000",
+                str(event.old.name) + " was previous workspace"
+            ]
+        )
 
-
-def on_window_focus(i3, e):
-    with mutex:
-        if DEBUG:
-            f.write(f"Len {len(lays)}\n".encode())
-            f.write(f"{lays}\n".encode())
-        global previous_focus
-        if previous_focus not in (-1, deleted_focus):
-            if DEBUG:
-                f.write("Previous focus found.\n".encode())
-            result = subprocess.run(['check_layout.sh'], stdout=subprocess.PIPE)
-            if DEBUG:
-                f.write(f"Current layout {result}.\n".encode())
-            if result.stdout.decode() == "am":
-                lays[previous_focus] = 1
+    def on_window_focus(self, i3conn, event):
+        with self.window_list_lock:
+            if self.window_list > 0:
+                previous_focus = next(reversed(self.window_list))
+                result = subprocess.run(
+                    ['check_layout.sh'], stdout=subprocess.PIPE
+                )
+                if result.stdout.decode() == "am":
+                    self.window_list[previous_focus] = 1
+                else:
+                    self.window_list[previous_focus] = 0
+            window_id = event.container.props.id
+            key = 0
+            subprocess.run(["setxkbmap", "-option", ""])
+            if window_id in self.window_list:
+                key = self.window_list[window_id]
+                if key == 0:
+                    subprocess.run(
+                        [
+                            "setxkbmap", "-layout", "us,am", "-variant",
+                            ",phonetic-alt", "-option",
+                            "ctrl:nocaps,grp:rctrl_switch"
+                        ]
+                    )
+                else:
+                    subprocess.run(
+                        [
+                            "setxkbmap", "-layout", "am,us", "-variant",
+                            "phonetic-alt,", "-option",
+                            "ctrl:nocaps,grp:rctrl_switch"
+                        ]
+                    )
+                del self.window_list[window_id]
             else:
-                lays[previous_focus] = 0
-        else:
-            if DEBUG:
-                f.write("Previous focus not found.\n".encode())
-        current_focus = i3.get_tree().find_focused().id
-        subprocess.run(["setxkbmap", "-option", ""])
-        if current_focus not in lays:
-            if DEBUG:
-                f.write(f"{current_focus} not found setting to us.\n".encode())
-            subprocess.run(
-                [
-                    "setxkbmap", "-layout", "us,am", "-variant",
-                    ",phonetic-alt", "-option", "ctrl:nocaps,grp:rctrl_switch"
-                ]
-            )
-            lays[current_focus] = 0
-            # print(current_focus)
-        else:
-            if DEBUG:
-                f.write(f"{current_focus} found.\n".encode())
-            val = lays[current_focus]
-            if val == 0:
                 subprocess.run(
                     [
                         "setxkbmap", "-layout", "us,am", "-variant",
@@ -66,45 +85,75 @@ def on_window_focus(i3, e):
                         "ctrl:nocaps,grp:rctrl_switch"
                     ]
                 )
-                if DEBUG:
-                    f.write(f"{current_focus} setting to us.\n".encode())
-            else:
-                subprocess.run(
-                    [
-                        "setxkbmap", "-layout", "am,us", "-variant",
-                        "phonetic-alt,", "-option",
-                        "ctrl:nocaps,grp:rctrl_switch"
-                    ]
-                )
-                if DEBUG:
-                    f.write(f"{current_focus} setting to am\n".encode())
-        subprocess.run(["pkill", "-RTMIN+10", "i3blocks"])
-        previous_focus = current_focus
+            self.window_list[window_id] = key
+            subprocess.run(["pkill", "-RTMIN+10", "i3blocks"])
+
+    def launch_i3(self):
+        self.i3.main()
+
+    def launch_server(self):
+        selector = selectors.DefaultSelector()
+
+        def accept(sock):
+            conn, addr = sock.accept()
+            selector.register(conn, selectors.EVENT_READ, read)
+
+        def read(conn):
+            data = conn.recv(1024)
+            if data == b'switch':
+                with self.window_list_lock:
+                    tree = self.i3.get_tree()
+                    windows = set(w.id for w in tree.leaves())
+                    for window_id in self.window_list[1:]:
+                        if window_id not in windows:
+                            self.window_list.remove(window_id)
+                        else:
+                            self.i3.command('[con_id=%s] focus' % window_id)
+                            break
+            elif not data:
+                selector.unregister(conn)
+                conn.close()
+
+        selector.register(self.listening_socket, selectors.EVENT_READ, accept)
+
+        while True:
+            for key, event in selector.select():
+                callback = key.data
+                callback(key.fileobj)
+
+    def run(self):
+        t_i3 = threading.Thread(target=self.launch_i3)
+        t_server = threading.Thread(target=self.launch_server)
+        for t in (t_i3, t_server):
+            t.start()
 
 
-def on_window_close(i3, e):
-    with mutex:
-        global deleted_focus
-        current_focus = e.container.id
-        if current_focus in lays:
-            del lays[current_focus]
-            deleted_focus = current_focus
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        prog='focus-last.py',
+        description='''
+        Focus last focused window.
 
+        This script should be launch from the .xsessionrc without argument.
 
-def on_workspace_focus(i3, e):
-    subprocess.run(
-        ["dunstify", "-t", "2000",
-         str(e.old.name) + " was previous workspace"]
+        Then you can bind this script with the `--switch` option to one of your
+        i3 keybinding.
+        '''
     )
+    parser.add_argument(
+        '--switch',
+        dest='switch',
+        action='store_true',
+        help='Switch to the previous window',
+        default=False
+    )
+    args = parser.parse_args()
 
-
-if DEBUG:
-    buffsize = 0
-    f = open("/tmp/i3_ipc.log", "wb", buffering=buffsize)
-
-i3.on("window::focus", on_window_focus)
-i3.on("window::close", on_window_close)
-i3.on("workspace::focus", on_workspace_focus)
-
-signal.signal(signal.SIGUSR1, handle_pdb)
-i3.main()
+    if not args.switch:
+        focus_watcher = FocusWatcher()
+        focus_watcher.run()
+    else:
+        client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        client_socket.connect(SOCKET_FILE)
+        client_socket.send(b'switch')
+        client_socket.close()
