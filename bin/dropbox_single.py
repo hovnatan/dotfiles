04add26d48aed7826faf1542238fbb09bb5dc677
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-06-11 20:51:20 +0400

    view save

---
+++ bin/dropbox_single.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import subprocess
+import time
+import re
+import pathlib
+import sys
+
+DROPBOX_CMDLINE = pathlib.Path.home() / "Dropbox/scripts/dropbox.py"
+
+subprocess.call([DROPBOX_CMDLINE, 'start'])
+while True:
+    status = subprocess.check_output([DROPBOX_CMDLINE,
+                                      'status']).decode(sys.stdout.encoding)
+    if re.search("Up to date", status):
+        break
