740070c6b3c5255a2804e30dc16a55d9da600afc
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-06-16 20:55:13 +0400

    lightdark

--- bin/cw.sh
+++ bin/cw.sh
@@ -6,7 +6,8 @@ echo -n "$COLOR" > ~/.my_colors
 xrdb -override ~/.dotfiles/.Xresources_grb_"$COLOR"
 i3-msg reload
 
-color_switcher.sh ~/.config/termite/config ~/.config/termite/option ~/.config/termite/color/$COLOR
-
+color_switcher.sh ~/.config/termite/config ~/.config/termite/option ~/.config/termite/colors/$COLOR
 killall -USR1 termite
 echo -n "set -U _reload_color_config ""$COLOR" | fish
+
+color_switcher.sh ~/.config/zathura/zathurarc ~/.config/zathura/option ~/.config/zathura/colors/$COLOR
