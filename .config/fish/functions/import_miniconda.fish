7c23198abb92b2b1df2a8c6abc5fae32e4542a50
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-06-25 16:59:04 +0400

    conda config

--- .config/fish/functions/import_miniconda.fish
+++ .config/fish/functions/import_miniconda.fish
@@ -1,8 +1,3 @@
 function import_miniconda
-  if test -d ~/miniconda3/bin
-    set -U fish_user_paths ~/miniconda3/bin $fish_user_paths
-    source (conda info --root)/etc/fish/conf.d/conda.fish
-  else
-    echo "No miniconda3 distribution found"
-  end
+  eval /home/hovnatan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
