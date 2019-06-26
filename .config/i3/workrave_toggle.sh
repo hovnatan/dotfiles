0b07774e400eaec57f5d8d1a9d3d35891aeac13d
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-06-26 15:31:29 +0400

    workrave suspended

--- .config/i3/workrave_toggle.sh
+++ .config/i3/workrave_toggle.sh
@@ -3,7 +3,7 @@
 new_mode="normal"
 dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.GetOperationMode | grep normal
 if [ $? -eq 0 ]; then
-  new_mode="quiet"
+  new_mode="suspended"
 fi
 
 dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.SetOperationMode string:"${new_mode}"
