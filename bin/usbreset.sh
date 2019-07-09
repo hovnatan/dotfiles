cddc783e71e13d63400ded9798fb5bea9944315d
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-07-09 12:49:19 +0400

    sleep before layout

--- bin/usbreset.sh
+++ bin/usbreset.sh
@@ -6,5 +6,7 @@ sudo rmmod xhci_pci
 sudo modprobe ehci_pci
 sudo modprobe xhci_pci
 
+sleep 2
+
 setxkbmap -option ""
 setxkbmap -layout "us,am" -variant ",phonetic-alt" -option "ctrl:nocaps,grp:rctrl_switch"
