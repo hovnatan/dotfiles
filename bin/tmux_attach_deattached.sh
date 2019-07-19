40397e2abd2ab1866f2308aece4c1b0d72e58cc0
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-07-20 03:55:46 +0400

    tmux attach

--- bin/tmux_attach_deattached.sh
+++ bin/tmux_attach_deattached.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-session=$(tmux list-sessions | grep -v attached | head -n 1 | sed -rn 's/^([0-9]+):.*/\1/p')
+if [ -z $1 ]; then
+  session=$(tmux list-sessions | grep -v attached | head -n 1 | sed -rn 's/^([0-9]+):.*/\1/p')
+else
+  session=$1
+fi
+
 if [ -z $session ] ; then
   tmux
 else
