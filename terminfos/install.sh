bba993c5a93b32ab423ee251781f82a18d35e3e8
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-07-23 08:43:11 +0400

    tmux

--- terminfos/install.sh
+++ terminfos/install.sh
@@ -2,3 +2,9 @@
 
 tic -x -o $HOME/.terminfo tmux.terminfo
 tic -x -o $HOME/.terminfo termite.terminfo
+
+# compile latest ncurses with
+# ./configure --prefix=/home/hovnatan/opt
+# make
+# make install
+# then cp ~/opt/.../tmux-256color ~/.terminfo/t/
