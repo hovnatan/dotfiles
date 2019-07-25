aea38cc4448d9694f36f24d531c027a82aabce7b
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-07-25 17:41:32 +0400

    conda future

--- arch-setup.sh
+++ arch-setup.sh
@@ -64,7 +64,7 @@ bash ./Miniconda3-latest-Linux-x86_64.sh
 ln -s ~/.dotfiles/.condarc ~/.condarc
 conda update --all
 import_miniconda
-conda install pytorch torchvision opencv pillow yapf pylint jedi ipython rope boost tbb tbb-devel cmake conda-build Cython ipdb pudb sortedcontainers neovim matplotlib h5py scipy tensorboardx
+conda install pytorch torchvision opencv pillow yapf pylint jedi ipython rope boost tbb tbb-devel cmake conda-build Cython ipdb pudb sortedcontainers neovim matplotlib h5py scipy tensorboardx future
 
 conda install binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 # conda env export | cut -f 1 -d '=' > ~/.dotfiles/conda_environment.yml
