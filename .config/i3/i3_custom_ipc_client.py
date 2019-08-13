0b09acb840442a56ea17f601be3b30d9a1f5f300
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-08-13 19:11:10 +0400

    tmux load all ssh

--- .config/i3/i3_custom_ipc_client.py
+++ .config/i3/i3_custom_ipc_client.py
@@ -4,17 +4,12 @@ import socket
 import sys
 import os
 
-DEBUG = "DEBUG_I3_IPC" in os.environ
-
 SOCKET_FILE = '/tmp/i3_focus_last'
 
 if __name__ == '__main__':
     client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     client_socket.connect(SOCKET_FILE)
-    if DEBUG:
-        print(sys.argv[1])
     client_socket.send(sys.argv[1].encode())
     data = client_socket.recv(1024)
-    if DEBUG:
-        print(data.decode())
+    print(data.decode())
     client_socket.close()
