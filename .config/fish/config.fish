5ca70b32bb16b9d7286f57efa162e222f1be9d5b
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-09-17 18:03:00 +0400

    homebrew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -115,3 +115,11 @@ abbr htop "htop -u hovnatan"
 abbr p "python"
 abbr hm "history merge"
 abbr rfc "source ~/.config/fish/config.fish"
+
+set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+
