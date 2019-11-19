69db07ad375bef1300690860778604b9f81417a0
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-11-19 11:52:52 +0400

    freetype cleartype

--- arch-setup.sh
+++ arch-setup.sh
@@ -15,20 +15,6 @@ sudo luarocks install --server=http://luarocks.org/dev digestif
 
 sudo pip3 install pysnooper neovim-remote
 
-sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
-sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
-#Uncomment  export FREETYPE_PROPERTIES="truetype:interpreter-version=40"
-sudo nvim  /etc/profile.d/freetype2.sh
-
-#win fonts
-#wget https://aur.archlinux.org/cgit/aur.git/snapshot/ttf-ms-win10.tar.gz
-#tar xf ttf-ms-win10.tar.gz
-#cd ttf-ms-win10
-#mv ~/fonts/* .
-#makepkg -Acs
-#sudo pacman -U ttf-ms-win10 ttf-ms-win10-other
-
 sudo systemctl enable tlp.service
 sudo systemctl enable ntpd.service
 sudo systemctl enable NetworkManager.service
@@ -48,8 +34,23 @@ sudo nvim /etc/makepkg.conf
 # uncomment color
 sudo nvim /etc/pacman.conf
 wget https://www.dropbox.com/download?dl=packages/dropbox.py
-yay -S skype wd719x-firmware aic94xx-firmware zoom i3ipc-python-git libinput-gestures clipster xcwd-git foxitreader i3lock-color gruvbox-icon-theme python-torchvision bear-git nerd-fonts-dejavu-complete ripgrep-all python-snakeviz subliminal vte3-ng dragon-drag-and-drop-git google-chrome ptags xkb-switch-git flacon freeoffice
+yay -S skype wd719x-firmware aic94xx-firmware zoom i3ipc-python-git libinput-gestures clipster xcwd-git foxitreader i3lock-color gruvbox-icon-theme python-torchvision bear-git nerd-fonts-dejavu-complete ripgrep-all python-snakeviz subliminal vte3-ng dragon-drag-and-drop-git google-chrome ptags xkb-switch-git flacon freeoffice freetype2-cleartype
 sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
+
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+#Uncomment  export FREETYPE_PROPERTIES="truetype:interpreter-version=38"
+sudo nvim  /etc/profile.d/freetype2.sh
+
+#win fonts
+#wget https://aur.archlinux.org/cgit/aur.git/snapshot/ttf-ms-win10.tar.gz
+#tar xf ttf-ms-win10.tar.gz
+#cd ttf-ms-win10
+#mv ~/fonts/* .
+#makepkg -Acs
+#sudo pacman -U ttf-ms-win10 ttf-ms-win10-other
+
 
 sudo mkinitcpio -p linux
 
