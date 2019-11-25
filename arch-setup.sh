71e04de86240da108b87019cbdfe62418251c2ed
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-11-25 13:47:41 +0400

    move conda setup to separate script

--- arch-setup.sh
+++ arch-setup.sh
@@ -53,23 +53,6 @@ cd termite
 make
 sudo make install
 
-cd ~/Downloads
-aria2c -x 8 -s 8 https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash ./Miniconda3-latest-Linux-x86_64.sh
-ln -s ~/.dotfiles/.condarc ~/.condarc
-import_miniconda
-conda update --all
-conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy tensorboard pandas
-conda install yapf pylint jedi ipython rope ipdb pudb sortedcontainers neovim matplotlib h5py future prettytable easydict pynvim jupyter git nodejs
-conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
-# conda env export | cut -f 1 -d '=' > ~/.dotfiles/conda_environment.yml
-# conda env update --file ~/.dotfiles/conda_environment.yml
-ln -s /usr/local/share/terminfo/x/xterm-termite ~/miniconda3/share/terminfo/x/
-# run import_miniconda fish shell integration
-# for bash
-# run source /opt/miniconda3/etc/profile.d/conda.sh
-# conda activate
-
 # homebrew
 # cd ~
 # git clone https://github.com/Homebrew/brew
---
+++ conda_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~/Downloads
+aria2c -x 8 -s 8 https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh
+ln -s ~/.dotfiles/.condarc ~/.condarc
+import_miniconda
+conda update --all
+conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy tensorboard pandas
+conda install yapf pylint jedi ipython rope ipdb pudb sortedcontainers neovim matplotlib h5py future prettytable easydict pynvim jupyter git nodejs
+conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
+
+#also might be install sudo apt-get install -y libgl1-mesa-dev
+# conda env export | cut -f 1 -d '=' > ~/.dotfiles/conda_environment.yml
+# conda env update --file ~/.dotfiles/conda_environment.yml
+ln -s /usr/local/share/terminfo/x/xterm-termite ~/miniconda3/share/terminfo/x/
+# run import_miniconda fish shell integration
+# for bash
+# run source /opt/miniconda3/etc/profile.d/conda.sh
+# conda activate
+
