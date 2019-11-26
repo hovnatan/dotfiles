8516513b3a8403b1b2ee697e5362499b5f450f5c
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-11-26 22:56:34 +0400

    decrease zathura widht

--- bin/zathura_recent.sh
+++ bin/zathura_recent.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-options='-width -130'
+options='-width -120'
 
 pdfs=$(cat ~/.local/share/zathura/history | grep -Po '\[\K[^\]]*')
 index=$(echo "$pdfs" \
         | sed "s|^$HOME|~|" \
-        | grep -o -P '.{0,125}$' \
+        | grep -o -P '.{0,115}$' \
         | rofi -dmenu -i -p 'zathura history' -format i \
         ${options}
         )
