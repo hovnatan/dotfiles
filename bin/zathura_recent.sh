126e7e7fc2e4a4139b3b95d53cc80381a9b3fed8
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-11-26 12:33:24 +0400

    rofi case insensitive

--- bin/zathura_recent.sh
+++ bin/zathura_recent.sh
@@ -5,7 +5,7 @@ options='-width 90'
 selected=$(\
         cat ~/.local/share/zathura/history | grep -Po '\[\K[^\]]*' \
         | sed "s|^$HOME|~|" \
-        | rofi -dmenu -i -markup-rows \
+        | rofi -dmenu -i \
         ${options}
         )
 
