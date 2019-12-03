9117962202d4b49a01553a683bec01e5cd4bc45f
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-12-03 10:29:16 +0000

    install shapely and scikit-image

--- conda_setup.sh
+++ conda_setup.sh
@@ -6,7 +6,7 @@ bash ./Miniconda3-latest-Linux-x86_64.sh
 ln -s ~/.dotfiles/.condarc ~/.condarc
 import_miniconda
 conda update --all
-conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas
+conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas shapely scikit-image
 conda install yapf pylint jedi ipython rope ipdb pudb sortedcontainers neovim matplotlib h5py future prettytable easydict pynvim jupyter git nodejs
 conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 
