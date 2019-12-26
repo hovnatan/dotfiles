62cde08d51c77b5fca2e6cd2033affb27a1e3161
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-12-26 10:15:09 +0400

    wait until synced

--- bin/zathura_load.sh
+++ bin/zathura_load.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 INPUT_FILE="$HOME/Dropbox/zathura_save"
+SYNCED=""
+while [ "$SYNCED" == "" ] ; do
+  sleep 0.5
+  SYNCED=$("$HOME/Dropbox/scripts/dropbox.py" filestatus "$OUTPUT_FILE" | grep -- "up to date")
+done
 
 IFS=:
 cat "$INPUT_FILE" | while read line
