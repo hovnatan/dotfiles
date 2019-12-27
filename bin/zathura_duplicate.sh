d713b209f368452644942f00c065b73aef1150a2
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-12-27 18:25:46 +0400

    fix zathura_duplicate

--- bin/zathura_duplicate.sh
+++ bin/zathura_duplicate.sh
@@ -6,6 +6,10 @@ ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
 PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 
 if [ -n "$PID" ]; then
-  Z_CMD=$(ps -f --pid $PID | grep -Po 'zathura \K.*')
-  zathura "$Z_CMD"
+  FILENAME=$(dbus-send --print-reply --type=method_call --dest=org.pwmt.zathura.PID-$PID /org/pwmt/zathura \
+    org.freedesktop.DBus.Properties.Get string:org.pwmt.zathura string:filename | grep -oP ".*variant.*string\s+\"\K(.*)(?=\")")
+  PAGENUMBER=$(dbus-send --print-reply --type=method_call --dest=org.pwmt.zathura.PID-$PID /org/pwmt/zathura \
+    org.freedesktop.DBus.Properties.Get string:org.pwmt.zathura string:pagenumber | grep -oP ".*variant.*uint32\s+\K(.*)")
+  ((++PAGENUMBER))
+  zathura -P "$PAGENUMBER" "$FILENAME"
 fi
