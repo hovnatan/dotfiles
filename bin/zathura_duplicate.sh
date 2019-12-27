87cd7f614bb1cc5f250c56de82925910a36c74e2
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-12-27 18:07:24 +0400

    fix duplicate

--- bin/zathura_duplicate.sh
+++ bin/zathura_duplicate.sh
@@ -5,8 +5,7 @@ ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
 # Get PID of process whose window this is
 PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 
-# Get last child process (shell, vim, etc)
 if [ -n "$PID" ]; then
-  Z_CMD=$(ps -f --pid $PID | grep -Po 'zathura \K.*')
+  Z_CMD=$(ps -f --pid $PID | grep -Po 'zathura.*?\K\/.*')
   zathura "$Z_CMD"
 fi
