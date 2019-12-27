7b9391a6714831e36f6fe1a576fad4b1e5e79de7
Hovnatan Karapetyan <hovnatan@gmail.com>
2019-12-27 18:17:12 +0400

    zathura dup change

--- bin/zathura_duplicate.sh
+++ bin/zathura_duplicate.sh
@@ -6,6 +6,6 @@ ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
 PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 
 if [ -n "$PID" ]; then
-  Z_CMD=$(ps -f --pid $PID | grep -Po 'zathura.*?\K\/.*')
+  Z_CMD=$(ps -f --pid $PID | grep -Po 'zathura \K.*')
   zathura "$Z_CMD"
 fi
