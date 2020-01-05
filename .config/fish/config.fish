eeedd6feb403d4db09899bcbec97a21aadd6f833
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-01-05 11:32:46 +0400

    gnnof abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -100,6 +100,7 @@ abbr gaca "git add -u; git commit --amend --no-edit"
 abbr gd  "git diff"
 abbr gah 'git stash; and git pull --rebase; and git stash pop'
 abbr gm 'nvim (git rev-parse --show-toplevel)/.git/index'
+abbr gnof "git merge --no-commit --no-ff"
 abbr ta  "tmux_attach_deattached.sh"
 abbr tl  "tmux list-sessions"
 abbr tc "tmux capture-pane -pJ -S - | nvim -R '+set ft=log|set nowrap|set foldlevel=99|DisableWhitespace' '+norm G' --"
