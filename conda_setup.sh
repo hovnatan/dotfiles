1223a6f7e5ffef69a76d373fce60df6820a9717e
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-01-09 08:35:59 +0400

    setup improve

--- conda_setup.sh
+++ conda_setup.sh
@@ -10,6 +10,11 @@ conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-bui
 conda install yapf pylint jedi ipython rope ipdb pudb sortedcontainers matplotlib h5py future prettytable easydict pynvim jupyter
 conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 
+# pypng for indexed png, gitpython python wrapper for git
+pip install pypng gitpython tensorboard
+
+cp ~/miniconda3/share/fzf/shell/key-bindings.fish ~/.config/fish/functions/fzf_key_bindings.fish
+
 #also might be install sudo apt-get install -y libgl1-mesa-dev
 # conda env export | cut -f 1 -d '=' > ~/.dotfiles/conda_environment.yml
 # conda env update --file ~/.dotfiles/conda_environment.yml
@@ -18,6 +23,9 @@ conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_l
 # for bash
 # run source /opt/miniconda3/etc/profile.d/conda.sh
 # conda activate
+<<<<<<< HEAD
 
 # pypng for indexed png, gitpython python wrapper for git
 pip install pypng gitpython tensorboard
+=======
+>>>>>>> setup improve
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3,3 +3,6 @@
 wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
 mv nvim.appimage ~/.dotfiles/bin/nvim
 chmod +x ~/.dotfiles/bin/nvim
+
+wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd-musl_7.4.0_amd64.deb
+sudo dpkg -i fd-musl_7.4.0_amd64.deb
