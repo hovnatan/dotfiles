1fc87bfa9f7cf93321cb61e9310c41eadceffb08
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-01-16 17:55:30 +0400

    use kitty

--- .profile
+++ .profile
@@ -6,6 +6,8 @@ export QT_QPA_PLATFORMTHEME=gtk2
 export FREETYPE_PROPERTIES="truetype:interpreter-version=38"
 export PKGEXT='.pkg.tar.zst'
 
+export TERMINAL_APP="termite"
+
 export PATH="$HOME/.dotfiles/bin:$PATH"
 
 export MAKEFLAGS="-j7"
--- bin/cw.sh
+++ bin/cw.sh
@@ -19,6 +19,7 @@ echo -n "$COLOR" > ~/.my_colors
 
 xrdb -override ~/.dotfiles/.Xresources_grb_"$COLOR"
 
+color_switcher.sh ~/.config/kitty/kitty.conf ~/.config/kitty/option ~/.config/kitty/colors/$COLOR
 color_switcher.sh ~/.config/termite/config ~/.config/termite/option ~/.config/termite/colors/$COLOR
 killall -USR1 termite
 echo -n "set -U _reload_color_config ""$COLOR" | fish
