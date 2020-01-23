d6ce2be149846789ce3d96692a9a475cbbd4caf2
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-01-23 18:50:13 +0400

    term app in profile

--- bin/update_com.sh
+++ bin/update_com.sh
@@ -3,4 +3,3 @@
 fish -c fisher
 "$HOME/.tmux/plugins/tpm/bin/update_plugins" all
 nvim -c PlugUpdate
-yay -Syu
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6,3 +6,5 @@ chmod +x ~/.dotfiles/bin/nvim
 
 wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd-musl_7.4.0_amd64.deb
 sudo dpkg -i fd-musl_7.4.0_amd64.deb
+
+sudo apt install gawk
