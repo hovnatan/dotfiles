5a8bee651354cccbaf3d32166a862fa25dc952c4
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-01-29 14:19:52 +0400

    ignore ynt

--- bin/toggle_ynt.py
+++ bin/toggle_ynt.py
@@ -1,16 +1,20 @@
 import configparser
 import os
+import subprocess
 
 FILE = os.getenv('HOME') + '/.dotfiles/.config/dunst/dunstrc'
 config = configparser.ConfigParser()
 config.read(FILE)
 
 ENTRY = 'ignore_ynt'
+SUMMARY_TO_CHECK = 'Ընտանիք'
 
 if ENTRY in config:
     del config[ENTRY]
+    subprocess.run(['dunstify', '-t', '5000', f'Turned off {SUMMARY_TO_CHECK}'])
 else:
-    config[ENTRY] = {'summary': 'Ընտանիք', 'format': ""}
+    config[ENTRY] = {'summary': SUMMARY_TO_CHECK, 'format': ""}
+    subprocess.run(['dunstify', '-t', '5000', f'Turned on {SUMMARY_TO_CHECK}'])
 
 with open(FILE, 'w') as configfile:
     config.write(configfile)
--- bin/toggle_ynt.sh
+++ bin/toggle_ynt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python $HOME/.dotfiles/bin/toggle_ynt.py
+sleep 5
 killall dunst
 dunst &
