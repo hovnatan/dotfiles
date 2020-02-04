3692ecf457d615d89c64fa1ce941194621299cff
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-02-04 11:09:12 +0400

    tldr

--- bin/set_w_name.sh
+++ bin/set_w_name.sh
@@ -4,9 +4,9 @@
 FOCUSED=$(wmctrl -lp | grep $(xprop -root | grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | sed 's/^0x/0x0/'))
 FPID=$(echo $FOCUSED | awk '{print $3}')
 FNME=$(echo $FOCUSED | cut -f -4  -d ' ' --complement)
-if [ "$1" == "" ]; then
+if [[ "$1" == "" ]]; then
   NEW_NAME=$(echo "$FNME" | rofi -sep '|' -dmenu -p "New name > ")
-  if [ "$NEW_NAME" == "" ]; then
+  if [[ "$NEW_NAME" == "" ]]; then
     exit 1
   fi
 else
