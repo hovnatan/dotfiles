e00bea5244966aa9492f00040cfd4a569c368cbf
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-02-08 22:25:49 +0400

    sleep on battery

--- check-i3lock.sh
+++ check-i3lock.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+TO_SLEEP=300
+CHECK_TIME=30
+I=0
+I_MAX=$((TO_SLEEP / CHECK_TIME))
+
 while true; do
-  sleep 30
+  sleep "$CHECK_TIME"
   xset dpms force off
+  if acpi -b | grep Discharging ; then
+    ((++I))
+    if [[ "$I" == "$I_MAX" ]]; then
+      systemctl suspend
+    fi
+  else
+    I=0
+  fi
 done
