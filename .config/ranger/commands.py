6284942b0385fe1d7f3a0c858e2f126cb9144dee
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-02-22 15:04:04 +0400

    ranger autojump

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -11,7 +11,6 @@ class fzf_select(Command):
 
     See: https://github.com/junegunn/fzf
     """
-
     def execute(self):
         import subprocess
         import os.path
@@ -25,3 +24,21 @@ class fzf_select(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+
+class autojump_select(Command):
+    """
+    :select a dir from autojump list using fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        fzf = self.fm.execute_command(
+            "cat ~/.local/share/autojump/autojump.txt | awk '{print $2}' |  fzf +m +s",
+            universal_newlines=True,
+            stdout=subprocess.PIPE
+        )
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_dir = os.path.abspath(stdout.rstrip('\n'))
+            self.fm.cd(fzf_dir)
