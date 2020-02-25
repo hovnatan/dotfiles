80a883c48ddbca64d4a888ba7574faf59bfcab0d
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-02-25 17:49:10 +0400

    xkb switch

--- bin/setkmap.sh
+++ bin/setkmap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-setxkbmap -option ""
-setxkbmap -layout "us,am" -variant ",phonetic-alt" -option "ctrl:nocaps,grp:rctrl_switch"
+# setxkbmap -option ""
+# setxkbmap -layout "us,am" -variant ",phonetic-alt" -option "ctrl:nocaps,grp:rctrl_switch"
 xkbcomp $HOME/.dotfiles/.config/xkb/config $DISPLAY
 
 sleep 5
