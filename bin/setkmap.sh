076aa22b2be389e260a19997c1014e175d4c3e7a
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-02-27 12:39:25 +0400

    revert to old kb switch

--- bin/setkmap.sh
+++ bin/setkmap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 setxkbmap -option ""
-setxkbmap -layout "us,am" -variant ",phonetic-alt"
-xkbcomp $HOME/.dotfiles/.config/xkb/config $DISPLAY
+setxkbmap -layout "us,am" -variant ",phonetic-alt" -option "grp:alt_space_toggle,ctrl:nocaps,grp:rctrl_switch"
+# xkbcomp $HOME/.dotfiles/.config/xkb/config $DISPLAY
 
 sleep 5
 
