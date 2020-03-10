a157151226a2607d7bacca9071a9b5907a3913de
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-03-10 08:41:35 +0400

    mpv add sub replay

---
+++ .config/mpv/scripts/sub-replay.lua
@@ -0,0 +1,32 @@
+-- Usage:
+--           a - replay the previous TIME_SHIFT seconds
+--           A - replay the previous TIME_SHIFT seconds with subtitles
+-- Status:
+--    Experimental.
+
+TIME_SHIFT=5
+
+function replay_previous_seconds(flag)
+  if flag == true
+  then
+    mp.set_property("sub-visibility", "yes")
+    hide_timer = mp.add_timeout(TIME_SHIFT, replay_finished)
+  end
+  mp.commandv("seek", -TIME_SHIFT, "relative")
+end
+
+function replay_previous_seconds_with_subtitles()
+    replay_previous_seconds(true)
+end
+
+function replay_finished()
+    mp.set_property("sub-visibility", "no")
+    hide_timer = nil
+end
+
+function init()
+    mp.add_key_binding("a", "replay-previous-sentence", replay_previous_seconds)
+    mp.add_key_binding("A", "replay-previous-sentence-with-subtitles", replay_previous_seconds_with_subtitles)
+end
+
+mp.register_event("file-loaded", init)
