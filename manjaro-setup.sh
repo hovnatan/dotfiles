de769675472e0de90fe88ccbe4d7a1ed53ba23fc
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-03-19 14:48:45 +0400

    fix i915

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo systemctl enable NetworkManager.service
 
-sudo pacman -Sy --needed networkmanager network-manager-applet os-prober openssh pepper-flash ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp tlp-rdw htop intel-gpu-tools libva-intel-driver libva-utils intel-media-driver xorg fortune-mod workrave feh mpv xclip neovim python-neovim fzf tmux youtube-dl fish intel-ucode grub glew vtk xf86-video-intel rofi dunst zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift i3-wm i3status perl-anyevent-i3 qt5-styleplugins python-gobject aws-cli unzip imagemagick scrot pulseaudio-alsa pulseaudio-bluetooth pulsemixer pulsemixer fuseiso inkscape gimp hunspell-en_US hplip avahi cups lm_sensors uget transmission-cli transmission-gtk wget ack pavucontrol sshfs bluez bluez-utils acpi alsa-utils sysstat i3blocks texlive-core texlive-langextra pandoc xorg-xinit pdfgrep xbindkeys ripgrep aria2 tk libjpeg-turbo ctags time yarn w3m elinks odt2txt mediainfo highlight ffmpegthumbnailer atool yapf pandoc-citeproc luarocks tcl tk libimagequant dex mps-youtube fwupd vlc bash-language-server xcape lsof rsync unrar perl-image-exiftool meld libvdpau-va-gl vulkan-intel ntp jre-openjdk unclutter xss-lock pacman-contrib libappindicator-gtk3 blueman udisks2 udiskie tldr rofimoji keepassxc ntfs-3g docker trayer
+sudo pacman -Sy --needed networkmanager network-manager-applet os-prober openssh pepper-flash ethtool lsb-release smartmontools x86_energy_perf_policy powertop tlp tlp-rdw htop intel-gpu-tools libva-intel-driver libva-utils intel-media-driver xorg fortune-mod workrave feh mpv xclip neovim python-neovim fzf tmux youtube-dl fish intel-ucode grub glew vtk xf86-video-intel rofi dunst zathura zathura-djvu zathura-ps zathura-pdf-mupdf redshift i3-wm i3status perl-anyevent-i3 qt5-styleplugins python-gobject aws-cli unzip imagemagick scrot pulseaudio-alsa pulseaudio-bluetooth pulsemixer pulsemixer fuseiso inkscape gimp hunspell-en_US hplip avahi cups lm_sensors uget transmission-cli transmission-gtk wget ack pavucontrol sshfs bluez bluez-utils acpi alsa-utils sysstat i3blocks texlive-core texlive-langextra pandoc xorg-xinit pdfgrep xbindkeys ripgrep aria2 tk libjpeg-turbo ctags time yarn w3m elinks odt2txt mediainfo highlight ffmpegthumbnailer atool yapf pandoc-citeproc luarocks tcl tk libimagequant dex mps-youtube fwupd vlc bash-language-server xcape lsof rsync unrar perl-image-exiftool meld libvdpau-va-gl vulkan-intel ntp jre-openjdk unclutter xss-lock pacman-contrib libappindicator-gtk3 blueman udisks2 udiskie tldr rofimoji keepassxc ntfs-3g docker trayer termite
 
 sudo pacman -Sy --needed base-devel clang llvm cmake gdb valgrind perf strace cppcheck ccache eigen3 boost
 
@@ -27,25 +27,11 @@ git clone https://github.com/hovnatan/i3-volume.git
 
 yay -S --needed skype zoom dropbox
 yay -S --needed google-chrome freeoffice
-yay -S --needed manjaro-firmware i3ipc-python-git libinput-gestures clipster i3lock-color gruvbox-icon-theme bear-git nerd-fonts-dejavu-complete python-snakeviz vte3-ng
+yay -S --needed manjaro-firmware i3ipc-python-git libinput-gestures clipster i3lock-color gruvbox-icon-theme bear-git nerd-fonts-dejavu-complete python-snakeviz
 yay -S --needed dragon-drag-and-drop-git xkb-switch-git flacon mpv-mpris-git playerctl-git ranger-git autojump python-pynput fd
 # yay -S --needed ptags ripgrep-all
 # sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# hibernate
-# sudo fallocate -l 25G /swapfile
-# sudo chmod 0600 /swapfile
-# sudo mkswap /swapfile
-# sudo swapon /swapfile
-# sudo sh -c 'echo -n "/swapfile none swap defaults 0 0" >> /etc/fstab'
-# edit /etc/default/grub to have GRUB_CMDLINE_LINUX_DEFAULT="resume=UUID=c30e9703-6e2b-4374-acb2-f0f6b60b8e0a resume_offset=30332928"
-# where offset can be get from
-# sudo filefrag -v /swapfile | awk '{ if($1=="0:"){print $4} }'
-# and UUID is / partitions from /etc/fstab
-# sudo nvim /etc/mkinitcpio.conf
-# add in HOOKS resume before filesystems
-# sudo mkinitcpio -p linux-lts
-# sudo grub-mkconfig -o /boot/grub/grub.cfg
 # sudo nvim /etc/systemd/sleep.conf
 # AllowSuspendThenHibernate=yes
 # HibernateDelaySec=7200
