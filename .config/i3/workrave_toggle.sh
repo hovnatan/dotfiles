6df26cc893177866f6b67e6ee02fa147d1511090
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-03-23 14:28:59 +0400

    workrave suspend for x minutes

--- .config/i3/workrave_toggle.sh
+++ .config/i3/workrave_toggle.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-new_mode="normal"
 dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.GetOperationMode | grep normal
-if [ $? -eq 0 ]; then
-  new_mode="quiet"
+if [[ "$?" != 0 ]]; then
+  dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.SetOperationMode string:normal
+  dunstify -t 1000 "Turn on Workrave"
+  pkill -f -9 "bash.*workrave_toggle.sh"
+else
+  dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.SetOperationMode string:quiet
+  INDEX=$(echo "15 minutes|30 minutes|1 hour|2 hours" | rofi -width -75 -sep '|' -dmenu -p "Suspend Workrave for > " -format i)
+  TIME=900
+  case INDEX in
+    1)
+      TIME=900
+      ;;
+    2)
+      TIME=1800
+      ;;
+    3)
+      TIME=3600
+      ;;
+    4)
+      TIME=7200
+      ;;
+  esac
+  dunstify -t 1000 "Turn off Workrave for $TIME seconds."
+  sleep $TIME
+  dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.SetOperationMode string:normal
+  dunstify -t 1000 "Turn on Workrave"
 fi
 
-dbus-send --print-reply --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.SetOperationMode string:"${new_mode}"
