e7bb452663762ac4443674d736a1825e39a8a70f
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-04-07 19:05:56 +0400

    increase mpv cache

--- bin/setkmap.sh
+++ bin/setkmap.sh
@@ -4,6 +4,6 @@
 # setxkbmap -layout "us,am" -variant ",phonetic-alt" -option "grp:alt_space_toggle,ctrl:nocaps,grp:rctrl_switch"
 # xkbcomp $HOME/.dotfiles/.config/xkb/config $DISPLAY
 
-sleep 5
-killall -9 xcape
-xcape -e "Control_L=Escape"
+# sleep 5
+# killall -9 xcape
+# xcape -e "Control_L=Escape"
