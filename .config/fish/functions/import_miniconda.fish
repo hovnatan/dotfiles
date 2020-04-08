ec27f8ad71eac2e388098dc68f144c6e7987adcb
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-04-08 10:45:43 +0400

    conda loading

--- .config/fish/functions/import_miniconda.fish
+++ .config/fish/functions/import_miniconda.fish
@@ -1,7 +1,11 @@
 function import_miniconda
-  eval /home/hovnatan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-  bass source /home/hovnatan/miniconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
-  bass source /home/hovnatan/miniconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
-  bass source /home/hovnatan/miniconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
-  bass source /home/hovnatan/miniconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
+  if not string match -q "*miniconda3*" $PATH
+    eval /home/hovnatan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    bass source /home/hovnatan/miniconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
+    bass source /home/hovnatan/miniconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
+    bass source /home/hovnatan/miniconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
+    bass source /home/hovnatan/miniconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
+  else
+    echo "miniconda3 already in PATH."
+  end
 end
