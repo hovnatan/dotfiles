b7adb8c31ca92932de7dc89944daa8b6052d7181
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-07-29 10:48:52 +0400

    add cvxpy

--- conda_setup.sh
+++ conda_setup.sh
@@ -6,7 +6,7 @@ bash ./Miniconda3-latest-Linux-x86_64.sh
 ln -s ~/.dotfiles/.condarc ~/.condarc
 import_miniconda
 conda update --all
-conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas shapely scikit-image imgaug pycairo
+conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas shapely scikit-image imgaug pycairo cvxpy
 conda install yapf pylint jedi ipython rope ipdb pudb sortedcontainers matplotlib h5py future prettytable easydict pynvim jupyter colorama pyyaml numba cachetools packaging
 conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 
