87e42fe784fa2248dbd191ced526c0a781ae7b6f
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-07-29 09:22:50 +0400

    improve z save

--- bin/zathura_load.sh
+++ bin/zathura_load.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
 INPUT_DIR="$HOME/Dropbox/scripts/zathura/saves"
-if [[ "$1" == "" || "$1" == "load_from_rofi" ]]; then
-  FILES=""
-  FILEPATHS=()
-  FILENAMES=$()
-
-  while IFS= read -r FILENAME
-  do
-    FILEPATH="$INPUT_DIR/$FILENAME"
-    FILEPATHS+=( "$FILEPATH" )
-    MOD_TIME=$(date -r "$FILEPATH" "+%m/%d/%Y %H:%M:%S")
-    printf -v TEXT "%-35s%37s" "$FILENAME" "$MOD_TIME"
-    FILES="$FILES|$TEXT"
-  done <<< $(/usr/bin/ls -1t --color=no "$INPUT_DIR")
-
-  FILES="${FILES:1}"
-  INDEX=$(echo "$FILES" | rofi -width -75 -sep '|' -dmenu -p "Load filename > " -format i)
-  if [[ $INDEX != "" ]]; then
-    INPUT_FILE=${FILEPATHS[$INDEX]}
-  fi
-else
-  INPUT_FILE="$1"
-fi
+INPUT_FILE="$INPUT_DIR/$1"
 
 if [ ! -f "$INPUT_FILE" ]; then
-  dunstify -t 5000 "Wrong filename $INPUT_FILE."
   exit 1
 fi
 
-i3-msg layout tabbed
-
 IFS=:
-cat "$INPUT_FILE" | while read line
+cat "$INPUT_FILE" | while read LINE
 do
-  read filename pagenumber workspace name <<< "$line"
+  read filename pagenumber <<< "$LINE"
   pagenumber="${pagenumber#"${pagenumber%%[!0]*}"}" # remove leading zeros
   if [ "$pagenumber" == "" ]; then
     pagenumber=0
@@ -48,5 +24,4 @@ do
       break
     fi
   done
-  $HOME/.dotfiles/bin/set_w_name.sh "$name"
 done
--- bin/zathura_recent.sh
+++ bin/zathura_recent.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
-options='-width -120'
-
-pdfs=$(cat ~/.local/share/zathura/history | grep -Po '\[\K[^\]]*')
-index=$(echo "$pdfs" \
-        | sed "s|^$HOME|~|" \
-        | grep -o -P '.{0,115}$' \
-        | rofi -dmenu -i -p 'zathura history' -format i \
-        ${options}
-        )
-
-# exit if nothing is selected
-[[ -z $index ]] && exit
-
-((++index))
-selected=$(echo "$pdfs" | sed -n "$index"p)
-
-zathura "$selected" &
+PDFS=$(cat ~/.local/share/zathura/history | grep -Po '\[\K[^\]]*' | grep -o -P '.{0,115}$')
+echo "$PDFS"
+read INDEX
+((++INDEX))
+SELECTED=$(echo "$PDFS" | sed -n "$INDEX"p)
+zathura "$SELECTED" &
