4f0a75735bb917c9218ae47c421e2fcbff632c11
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-07-29 09:15:24 +0400

    improve z save

--- bin/zathura_save.sh
+++ bin/zathura_save.sh
@@ -1,78 +1,25 @@
 #!/bin/bash
 
-PIDS_WS_NAME=$(wmctrl -lp)
-if [ "$PIDS_WS_NAME" == "" ]; then
-  exit 0
-fi
+PIDS=$(wmctrl -lp | awk '{print $3}')
+
 OUTPUT_DIR="$HOME/Dropbox/scripts/zathura/saves"
-if [ "$1" == "" ]; then
-  FILES=""
-  for filename in "$OUTPUT_DIR"/*; do
-    fbase=$(basename "$filename")
-    FILES="$FILES|$fbase"
-  done
-  FILES="${FILES:1}"
-  OUTPUT_FILE=$(echo "$FILES" | rofi -sep '|' -dmenu -p "Save to filename > ")
-  if [ "$OUTPUT_FILE" == "" ]; then
-    dunstify -t 5000 "Wrong filename."
-    exit 1
-  fi
-else
-  OUTPUT_FILE="$1"
-  if [ "$2" != "" ]; then
-    OUTPUT_FILE="${OUTPUT_FILE}_$(hostname)"
-  fi
-fi
+OUTPUT_FILE="$1"
+OUTPUT_FILE="${OUTPUT_FILE}_$(hostname)"
 OUTPUT_FILE="$OUTPUT_DIR/$OUTPUT_FILE"
+rm "$OUTPUT_FILE"
 
-OUTPUT_FILE_NAMES=()
-IFS=:
-while read line
+while read PID
 do
-  read PID WS NAME <<< "$line"
-  if [[ "$1" == "last" ]]; then
-    OUTPUT_FILE_NAME="${OUTPUT_FILE}_$WS"
-  else
-    OUTPUT_FILE_NAME="${OUTPUT_FILE}"
+  ps -p $PID | grep zathura
+  if [[ $? != 0 ]] ; then
+    continue
   fi
-  if [[ ! " ${OUTPUT_FILE_NAMES[@]} " =~ " ${OUTPUT_FILE_NAME} " ]]; then
-    OUTPUT_FILE_NAMES+=( "$OUTPUT_FILE_NAME" )
-    rm -f "$OUTPUT_FILE_NAME"
-    touch "$OUTPUT_FILE_NAME"
-  fi
-
   filename=$(dbus-send --print-reply --type=method_call --dest=org.pwmt.zathura.PID-$PID /org/pwmt/zathura \
     org.freedesktop.DBus.Properties.Get string:org.pwmt.zathura string:filename | grep -oP ".*variant.*string\s+\"\K(.*)(?=\")")
   pagenumber=$(dbus-send --print-reply --type=method_call --dest=org.pwmt.zathura.PID-$PID /org/pwmt/zathura \
     org.freedesktop.DBus.Properties.Get string:org.pwmt.zathura string:pagenumber | grep -oP ".*variant.*uint32\s+\K(.*)")
   printf -v OUTPUT_PAGENUMBER "%05d" $pagenumber
-  echo "$filename:$OUTPUT_PAGENUMBER:$WS:$NAME" >> "$OUTPUT_FILE_NAME"
-done <<< "$PIDS_WS_NAME"
+  echo "$filename:$OUTPUT_PAGENUMBER" >> "$OUTPUT_FILE"
+done <<< "$PIDS"
 
-for OUTPUT_FILE_NAME in "${OUTPUT_FILE_NAMES[@]}"; do
-  sort -o "$OUTPUT_FILE_NAME" "$OUTPUT_FILE_NAME"
-done
-
-COUNT_TO_SYNC=${#OUTPUT_FILE_NAMES[@]}
-SYNCED_FILES=()
-i=0
-while [ ${#SYNCED_FILES[@]} != $COUNT_TO_SYNC ] && [ $i -lt 30 ] ; do
-  sleep 0.5
-  for OUTPUT_FILE_NAME in "${OUTPUT_FILE_NAMES[@]}"; do
-    if [[ " ${SYNCED_FILES[@]} " =~ " ${OUTPUT_FILE_NAMES} " ]]; then
-      continue
-    fi
-    STATUS=$("dropbox.py" filestatus "$OUTPUT_FILE_NAME" | grep -- "up to date")
-    if [ "$STATUS" != "" ]; then
-      SYNCED_FILES+=( "$OUTPUT_FILE_NAME" )
-    fi
-  done
-  ((i=i+1))
-done
-
-if [ ${#SYNCED_FILES[@]} != $COUNT_TO_SYNC ]; then
-  dunstify -t 5000 -u c "Couldn't Dropbox sync zathura file at $OUTPUT_FILE."
-else
-  dunstify -t 5000 "Dropbox synced zathura file at $OUTPUT_FILE."
-fi
-sleep 1
+sort -o "$OUTPUT_FILE" "$OUTPUT_FILE"
