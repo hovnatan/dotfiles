3ecfa6f059ca9e2b467c66cb9b48d4c22d76da65
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-07-30 11:46:14 +0400

    Add octave

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -6,7 +6,7 @@ sudo apt-add-repository ppa:fish-shell/release-3
 
 sudo apt-get update
 
-sudo apt-get install -y software-properties-common jq feh w3m-img tmux fzf fd-find ripgrep fish ranger clang clangd bear ppa-purge valgrind curl neovim htop
+sudo apt-get install -y software-properties-common jq feh w3m-img tmux fzf fd-find ripgrep fish ranger clang clangd bear ppa-purge valgrind curl neovim htop octave
 
 # sudo apt-get install -y texlive-latex-extra latexmk chrome-gnome-shell zathura xrdp fonts-croscore kitty
 # sudo apt-get install -y i3 i3blocks
