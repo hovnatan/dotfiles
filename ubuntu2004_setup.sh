32756a531c5480ab79807c50d8c1848e22d9e060
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-08-06 08:43:05 +0400

    remove zathura completion

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -30,3 +30,5 @@ pip3 install --user pylint yapf jedi neovim ueberzug
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+
+sudo rm /usr/share/fish/completions/zathura.fish
