b359b2cbed34506680f15f35a5313c12692e1aeb
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-09-11 09:26:46 +0400

    version bump

--- conda_setup.sh
+++ conda_setup.sh
@@ -5,10 +5,10 @@
 # bash ./Miniconda3-latest-Linux-x86_64.sh
 # ln -s ~/.dotfiles/.condarc ~/.condarc
 # import_miniconda
-conda update --all
-conda install pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas shapely scikit-image imgaug pycairo cvxpy cvxopt
-conda install yapf pylint jedi ipython rope ipdb pudb sortedcontainers matplotlib h5py future prettytable easydict pynvim jupyter colorama pyyaml numba cachetools packaging pytest coverage twine sphinx
-conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
+conda update -y --all
+conda install -y pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas shapely scikit-image imgaug pycairo cvxpy cvxopt
+conda install -y yapf pylint jedi ipython rope ipdb pudb sortedcontainers matplotlib h5py future prettytable easydict pynvim jupyter colorama pyyaml numba cachetools packaging pytest coverage twine sphinx boto3
+# conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 
 # # pypng for indexed png, gitpython python wrapper for git
 # pip install pypng gitpython tensorboard
