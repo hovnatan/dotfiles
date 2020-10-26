1503732a266a4b840060381f038fb3b498aebc87
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-10-26 09:42:31 +0400

    remove snap

--- bin/update_com.sh
+++ bin/update_com.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [[ "$1" != "" ]] ; then
-  fish -c fisher &
-fi
-"$HOME/.tmux/plugins/tpm/bin/update_plugins" all &
-
-wait
-
 sudo apt update
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
+
+if [[ "$1" != "" ]] ; then
+  fish -c fisher
+fi
+
+"$HOME/.tmux/plugins/tpm/bin/update_plugins" all
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate
