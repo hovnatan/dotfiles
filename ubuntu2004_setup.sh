f5d7b76556660d21a7afce41f03faecdaa8a45b1
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-10-28 14:09:44 +0400

    Add video folder upload

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 
 sudo apt-get install -y software-properties-common jq feh w3m-img tmux fzf fd-find ripgrep fish ranger clang clangd clang-format bear ppa-purge valgrind curl neovim htop octave libfreetype6-dev libfontconfig1-dev libxext-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev build-essential libxft-dev net-tools sshfs cmake rustc aria2 mediainfo
 
-sudo apt-get install -y meld tightvncserver
+sudo apt-get install -y meld xrdp remmina zathura
 
 # sudo apt-get install -y texlive-latex-extra latexmk chrome-gnome-shell zathura xrdp fonts-croscore kitty
 # sudo apt-get install -y i3 i3blocks
