54f4503f645332a06883cba5a4e2c25d1d15e580
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-12-03 14:56:25 +0400

    docker compose

--- conda_setup.sh
+++ conda_setup.sh
@@ -7,7 +7,7 @@
 # import_miniconda
 conda update -y --all
 conda install -y pytorch torchvision opencv pillow boost matplotlib scipy conda-build Cython sympy pandas shapely scikit-image imgaug pycairo cvxpy cvxopt scikit-learn
-conda install -y yapf pylint jedi ipython rope ipdb pudb sortedcontainers matplotlib h5py future prettytable easydict pynvim jupyter colorama pyyaml numba cachetools packaging pytest pytest-xdist pytest-cov coverage twine sphinx boto3
+conda install -y yapf pylint jedi ipython rope ipdb pudb sortedcontainers matplotlib h5py future prettytable easydict pynvim jupyter colorama pyyaml numba cachetools packaging pytest pytest-xdist pytest-cov coverage twine sphinx boto3 docker-compose
 # conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 
 # # pypng for indexed png, gitpython python wrapper for git
