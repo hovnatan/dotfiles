df66bf878322de7d52756ffbf19dcf97a19485b0
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-12-05 11:48:15 +0400

    clang tidy

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository restricted
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 
-sudo apt-get install -y software-properties-common jq feh w3m-img tmux fzf fd-find ripgrep fish ranger clang clangd clang-format bear ppa-purge valgrind curl neovim htop octave libfreetype6-dev libfontconfig1-dev libxext-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev build-essential libxft-dev net-tools sshfs cmake rustc cargo aria2 mediainfo xdg-utils wmctrl awscli docker.io default-jre universal-ctags
+sudo apt-get install -y software-properties-common jq feh w3m-img tmux fzf fd-find ripgrep fish ranger clang clangd clang-format clang-tidy bear ppa-purge valgrind curl neovim htop octave libfreetype6-dev libfontconfig1-dev libxext-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev build-essential libxft-dev net-tools sshfs cmake rustc cargo aria2 mediainfo xdg-utils wmctrl awscli docker.io default-jre universal-ctags
 
 sudo apt-get install -y meld xrdp remmina zathura
 
