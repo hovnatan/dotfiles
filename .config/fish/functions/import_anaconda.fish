804868308ee14f8229a0d4fe6a09705864c393be
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-12-07 16:07:43 +0400

    ssh agent

--- .config/fish/functions/import_anaconda.fish
+++ .config/fish/functions/import_anaconda.fish
@@ -1,10 +1,10 @@
 function import_anaconda
   if not string match -q "*anaconda3*" $PATH
     eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-    bax source ~/anaconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
-    bax source ~/anaconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
-    bax source ~/anaconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
-    bax source ~/anaconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
+    replay source ~/anaconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
+    replay source ~/anaconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
+    replay source ~/anaconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
+    replay source ~/anaconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
   else
     echo "anaconda3 already in PATH."
   end
--- .config/fish/functions/import_miniconda.fish
+++ .config/fish/functions/import_miniconda.fish
@@ -1,10 +1,10 @@
 function import_miniconda
   if not string match -q "*miniconda3*" $PATH
     eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    bax source ~/miniconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
-    bax source ~/miniconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
-    bax source ~/miniconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
-    bax source ~/miniconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
+    replay source ~/miniconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
+    replay source ~/miniconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
+    replay source ~/miniconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
+    replay source ~/miniconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
   else
     echo "miniconda3 already in PATH."
   end
