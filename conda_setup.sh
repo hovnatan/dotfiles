ca770968bab78615303a72e6ee9b67aa6fe48150
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-12-21 13:36:57 +0400

    Version bump

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~/Downloads
+rm -rf Miniconda3-latest-Linux-x86_64.sh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh
 
