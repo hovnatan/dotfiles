3bf3bb1ad9ab90f203881d36341d2e3adebf56d5
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-01-08 14:01:34 +0400

    better miniconda import

--- .config/fish/functions/import_anaconda.fish
+++ .config/fish/functions/import_anaconda.fish
@@ -1,10 +1,12 @@
 function import_anaconda
   if not string match -q "*anaconda3*" $PATH
-    eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-    replay source ~/anaconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
-    replay source ~/anaconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
-    replay source ~/anaconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
-    replay source ~/anaconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
+    source ~/anaconda3/etc/fish/conf.d/conda.fish
+    conda activate base
+    # eval ~/anaconda3/bin/conda "shell.fish" "hook" | source
+    # replay source ~/anaconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
+    # replay source ~/anaconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
+    # replay source ~/anaconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
+    # replay source ~/anaconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
   else
     echo "anaconda3 already in PATH."
   end
--- .config/fish/functions/import_miniconda.fish
+++ .config/fish/functions/import_miniconda.fish
@@ -1,10 +1,12 @@
 function import_miniconda
   if not string match -q "*miniconda3*" $PATH
-    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    replay source ~/miniconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
-    replay source ~/miniconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
-    replay source ~/miniconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
-    replay source ~/miniconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
+    source ~/miniconda3/etc/fish/conf.d/conda.fish
+    conda activate base
+    # eval ~/miniconda3/bin/conda "shell.fish" "hook" | source
+    # replay source ~/miniconda3/etc/conda/activate.d/activate-gcc_linux-64.sh
+    # replay source ~/miniconda3/etc/conda/activate.d/activate-gxx_linux-64.sh
+    # replay source ~/miniconda3/etc/conda/activate.d/activate-binutils_linux-64.sh
+    # replay source ~/miniconda3/etc/conda/activate.d/nvcc_linux-64_activate.sh
   else
     echo "miniconda3 already in PATH."
   end
