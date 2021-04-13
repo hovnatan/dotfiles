0b5b2b4ab4b13d35f712f78679fa6de90ded85c2
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-04-13 17:02:22 +0400

    opencv contrib

--- mbp_setup.sh
+++ mbp_setup.sh
@@ -1,7 +1,9 @@
 sudo port selfupdate
 sudo port upgrade outdated
 
-sudo port install cmake tmux git neovim py-neovim fzf ripgrep yarn nodejs14 fish wget
+sudo port install cmake tmux git neovim py-neovim fzf ripgrep yarn nodejs14 fish wget opencv4
+
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 infocmp -x tmux-256color > xyz
 /usr/bin/tic -x xyz
