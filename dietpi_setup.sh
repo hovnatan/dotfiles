d53b33fc0e81123b166537916a70dc379ccb4a20
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-04-18 08:17:01 +0100

    http server

--- dietpi_setup.sh
+++ dietpi_setup.sh
@@ -1,15 +1,6 @@
 
-sudo apt-get -y install tmux fish openssh-client
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get -y install tmux fish openssh-client vim
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install -y yarn
-
-sudo apt-get install -y python2 python3-pip
-pip3 install --user pylint yapf jedi neovim ueberzug
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
