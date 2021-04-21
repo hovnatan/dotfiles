9a35676a413ce71d7723ee84fa0db49dfe1a67f6
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-04-21 14:29:25 +0400

    init

--- mbp_setup.sh
+++ mbp_setup.sh
@@ -1,13 +1,13 @@
 sudo port selfupdate
 sudo port upgrade outdated
 
-sudo port install cmake tmux git neovim py-neovim fzf ripgrep yarn fish wget ccache fd
+sudo port install tmux git neovim py-neovim fzf ripgrep yarn fish wget fd ranger
 
-sudo port install opencv4
+# sudo port install opencv4 ccache cmake
 
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-infocmp -x tmux-256color > xyz
-/usr/bin/tic -x xyz
-rm xyz
+# infocmp -x tmux-256color > xyz
+# /usr/bin/tic -x xyz
+# rm xyz
 
