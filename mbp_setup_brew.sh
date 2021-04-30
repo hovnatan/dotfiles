3c92fe130cf40f22616b1a5988daf1d935b3b44f
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-04-30 17:22:22 +0400

    homebrew

--- mbp_setup_brew.sh
+++ mbp_setup_brew.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+set -e
 
+$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew update
 
-brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish
-
+brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish ranger
 
 /usr/local/opt/ncurses/bin/infocmp tmux-256color > ~/tmux-256color.info
-tic -xe tmux-256color tmux-256color.info
+tic -xe tmux-256color ~/tmux-256color.info
+rm ~/tmux-256color.info
 
+pip3 install pynvim
