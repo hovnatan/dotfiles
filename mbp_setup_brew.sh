130bfce160a87d751f90fbe74d7ce560de7efe21
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-05-04 09:20:29 +0400

    cmake

--- mbp_setup_brew.sh
+++ mbp_setup_brew.sh
@@ -6,7 +6,7 @@ $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew update
 
-brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish ranger
+brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish ranger cmake
 
 /usr/local/opt/ncurses/bin/infocmp tmux-256color > ~/tmux-256color.info
 tic -xe tmux-256color ~/tmux-256color.info
