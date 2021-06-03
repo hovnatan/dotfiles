a7bc759815590d2c897388b73f1c29d01018babc
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-06-03 10:04:51 +0400

    Add digits image

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 cd ~/Downloads
-rm -rf Miniconda3-latest-Linux-x86_64.sh
 rm -rf ~/miniconda3
 
 VERSION="latest"
 FILENAME="Miniconda3-${VERSION}-Linux-x86_64.sh"
 
+rm -rf "$FILENAME"
 wget https://repo.anaconda.com/miniconda/$FILENAME
 bash ./$FILENAME -b
 
@@ -24,7 +26,7 @@ conda install -y jupyterlab
 # conda install tbb tbb-devel cmake binutils_impl_linux-64 binutils_linux-64 gxx_linux-64 gcc_linux-64 nvcc_linux-64 libx11-common-cos6-x86_64 libx11-cos6-x86_64 libxdamage-cos6-x86_64 libxfixes-cos6-x86_64 libxxf86vm-cos6-x86_64 mesa-dri-drivers-cos6-x86_64 mesa-dri1-drivers-cos6-x86_64 mesa-libgl-cos6-x86_64 mesa-libgl-devel-cos6-x86_64 libselinux-cos6-x86_64
 
 # pypng for indexed png, gitpython python wrapper for git
-pip install pypng gitpython tensorboard sphinx-rtd-theme pylint-json2html pylint-pytest pytest-timeout pyenchant pretty_errors scalene opencv-contrib-python
+pip install pypng gitpython tensorboard sphinx-rtd-theme pylint-json2html pylint-pytest pytest-timeout pyenchant pretty_errors scalene opencv-contrib-python reedsolo albumentations segmentation-models-pytorch kornia imagecorruptions imgaug imutils
 
 
 # also might be install sudo apt-get install -y libgl1-mesa-dev
