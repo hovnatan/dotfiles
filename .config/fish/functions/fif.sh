2dc0c2cbaeb87f072901d27370b735090e32b773
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-06-15 10:48:39 +0400

    man

---
+++ .config/fish/functions/fif.sh
@@ -0,0 +1,12 @@
+function fif
+    set -x RG_PREFIX rga --files-with-matches
+    set -l file
+    set file (
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv'" \
+            fzf --sort --preview="[ ! -z {} ] && rga --pretty --context 5 {q} {}" \
+                --phony -q "$argv" \
+                --bind "change:reload:$RG_PREFIX {q}" \
+                --preview-window="70%:wrap"
+    ) &&
+    open "$file"
+end
