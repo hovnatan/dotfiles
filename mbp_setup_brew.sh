9511c28baae62b774251fc8be884cc88b1982cae
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-06-30 13:39:23 +0400

    Try Vulkan

--- mbp_setup_brew.sh
+++ mbp_setup_brew.sh
@@ -6,9 +6,13 @@ $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew update
 
-brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish ranger wget less coreutils diffutils git bash gawk man-db dust bat graphviz htop conan alt-tab git-lfs bazel cargo
+brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish ranger wget less coreutils diffutils git bash gawk man-db dust bat graphviz htop conan alt-tab git-lfs bazel rustup-init
 brew install --cask cmake
 brew install rga pandoc poppler tesseract ffmpeg
+
+brew install gst-plugins-good gst-plugins-bad
+rustup-init
+cargo install hunter
 
 # /usr/local/opt/ncurses/bin/infocmp tmux-256color > ~/tmux-256color.info
 # tic -xe tmux-256color ~/tmux-256color.info
