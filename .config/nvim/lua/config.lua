921072ccca25c06175fe07b94c077585bf28d641
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-07-13 17:14:21 +0400

    Add formatter

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -403,3 +403,49 @@ require('gitsigns').setup {
 }
 
 vim.api.nvim_set_keymap('n', '$', "<cmd>lua require'hop'.hint_words()<cr>", {})
+
+require('formatter').setup({
+   logging = false,
+    filetype = {
+      javascript = {
+          -- prettier
+         function()
+            return {
+              exe = "prettier",
+              args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--single-quote'},
+              stdin = true
+            }
+          end
+      },
+      rust = {
+        -- Rustfmt
+        function()
+          return {
+            exe = "rustfmt",
+            args = {"--emit=stdout"},
+            stdin = true
+          }
+        end
+      },
+      lua = {
+          -- luafmt
+          function()
+            return {
+              exe = "luafmt",
+              args = {"--indent-count", 2, "--stdin"},
+              stdin = true
+            }
+          end
+        },
+      python = {
+          function()
+            return {
+              exe = "black",
+              args = {"-"},
+              stdin = true
+            }
+          end
+        }
+    }
+  }
+)
