79dbef1ec12189fb14a991c1161af872f6be3b37
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-07-13 17:55:47 +0400

    Add formatter

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -449,3 +449,9 @@ require('formatter').setup({
     }
   }
 )
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
