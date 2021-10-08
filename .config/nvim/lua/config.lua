0e8d6019fd9efef8694f0e003a61c0cfed5c46e6
Hovnatan Karapetyan <hovnatan@thinkpad>
2021-10-08 18:16:40 +0400

    Disable some treesitter

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -261,6 +261,7 @@ cmp.setup({
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })
     },
     sources = {
       { name = 'nvim_lsp' },
