646d772ba394128f86c5140a77e468a4af884074
Hovnatan Karapetyan <hovnatan@work>
2021-12-24 18:39:44 +0400

    Work on cmd

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -245,13 +245,6 @@ cmp.setup({
       { name = 'path' },
     }
   })
-cmp.setup.cmdline('/', {
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
   sources = cmp.config.sources({
     { name = 'path' }
