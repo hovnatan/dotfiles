38f570278afe436b58762e1e16ceea6aeab4a67c
Hovnatan Karapetyan <hovnatan@mobeus>
2022-01-21 16:51:00 +0400

    Fix camera on M1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,33 +9,7 @@ set -U FZF_FIND_FILE_COMMAND "fd -IHp --ignore-file ~/.config/fd/ignore . \$dir"
 set -U FZF_DEFAULT_COMMAND $FZF_FIND_FILE_COMMAND
 set -x EDITOR nvim
 
-function forward-word-or-exit
-    set -l cmd (commandline)
-    if test -n "$cmd"
-        commandline -f forward-word
-    else
-        exit
-    end
-end
-
-set -g fish_vi_force_cursor 1
-
-function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
-    for mode in default insert visual
-        fish_default_key_bindings -M $mode
-    end
-    fish_vi_key_bindings --no-erase
-    bind \cp up-or-search
-    bind \cn down-or-search
-    bind \cd forward-word-or-exit
-    bind -M insert \cd forward-word-or-exit
-#    bind -M insert -m default jk backward-char force-repaint
-#    bind -m insert \e force-repaint
-end
-
 set -u fish_term24bit 1
-
-set -g fish_key_bindings hybrid_bindings
 
 set -U fish_cursor_default block
 set -U fish_cursor_insert line
