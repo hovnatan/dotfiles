1e4a39f189af41464953b91448df91af3a84f626
Hovnatan Karapetyan <hovnatan@mobeus>
2022-02-23 11:02:38 +0400

    Start work on fix

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -102,7 +102,7 @@ require('telescope').setup{
 
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-    path_display={"smart"}
+    path_display={"truncate"},
   },
     pickers = {
       -- Your special builtin config goes in here
