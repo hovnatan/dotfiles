e2b350cfb5fef0d19380f21abe5424c9779e571e
Hovnatan Karapetyan <hovnatan@mbp>
2022-03-29 20:04:13 +0400

    Refactor

--- mbp_setup_brew.sh
+++ mbp_setup_brew.sh
@@ -8,16 +8,10 @@ brew update
 
 brew install llvm bear fzf fd ripgrep neovim gtk+3 nodejs tmux fish wget less coreutils diffutils git bash gawk man-db dust bat graphviz htop conan git-lfs bazel rustup-init go pyright findutils git-delta diskonaut exa
 brew install --cask cmake alacritty time-out
-brew install rga pandoc poppler tesseract ffmpeg
-
-brew install zathura-pdf-poppler
-brew install girara --HEAD
-brew install zathura --HEAD
-mkdir -p $(brew --prefix zathura)/lib/zathura
-ln -sf $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+brew install rga pandoc tesseract ffmpeg
 
 rustup-init
-cargo install hunter
+cargo install difftastic 
 
 # /usr/local/opt/ncurses/bin/infocmp tmux-256color > ~/tmux-256color.info
 # tic -xe tmux-256color ~/tmux-256color.info
