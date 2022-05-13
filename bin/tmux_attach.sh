a829b096826b89a9c69b6be9c4b3666149ff5353
Hovnatan Karapetyan <hovnatan@mbp>
2022-05-13 14:55:11 +0400

    CN

--- bin/tmux_attach.sh
+++ bin/tmux_attach.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  session="default"
+  session=$(tmux list-sessions | head -n 1 | cut -d: -f1)
+  if [ -z $session ]; then
+    session="default"
+  fi
 else
   session=$1
 fi
