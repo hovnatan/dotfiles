7974baec78e7d39eff00d2c4a3ec1aaf6e295812
Hovnatan Karapetyan <hovnatan@mbp>
2022-06-06 12:38:59 +0400

    stylua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,3 +29,53 @@ vim.g.netrw_maxfilenamelen = 50
 
 vim.o.spelllang = en_us
 vim.o.spellfile = "~/Dropbox/scripts/nvim/spell/en.utf-8.add"
+
+vim.cmd[[
+let s:savedCpo = &cpo
+set cpo&vim
+
+function! s:VStarsearch_searchCWord()
+	let wordStr = expand("<cword>")
+	if strlen(wordStr) == 0
+		echohl ErrorMsg
+		echo 'E348: No string under cursor'
+		echohl NONE
+		return
+	endif
+
+	if wordStr[0] =~ '\<'
+		let @/ = '\<' . wordStr . '\>'
+	else
+		let @/ = wordStr
+	endif
+
+	let savedUnnamed = @"
+	let savedS = @s
+	normal! "syiw
+	if wordStr != @s
+		normal! w
+	endif
+	let @s = savedS
+	let @" = savedUnnamed
+endfunction
+
+" https://github.com/bronson/vim-visual-star-search/
+function! s:VStarsearch_searchVWord()
+	let savedUnnamed = @"
+	let savedS = @s
+	normal! gv"sy
+	let @/ = '\V' . substitute(escape(@s, '\'), '\n', '\\n', 'g')
+	let @s = savedS
+	let @" = savedUnnamed
+endfunction
+
+nnoremap <silent> * :call <SID>VStarsearch_searchCWord()<CR>:set hls<CR>
+vnoremap <silent> * :<C-u>call <SID>VStarsearch_searchVWord()<CR>:set hls<CR>
+nmap <silent> # *
+vmap <silent> # *
+
+noremap <expr> n 'Nn'[v:searchforward]
+noremap <expr> N 'nN'[v:searchforward]
+
+let &cpo = s:savedCpo
+]]
