b852309e032053537ea1a5cb1b1063d31325953b
Hovnatan Karapetyan <hovnatan@mbp>
2022-06-13 14:35:10 +0400

    Start yaml config

--- .config/nvim/lua/modules/config/lsp/init.lua
+++ .config/nvim/lua/modules/config/lsp/init.lua
@@ -8,12 +8,6 @@ return function()
     local function buf_set_keymap(...)
       vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
-    local function buf_set_option(...)
-      vim.api.nvim_buf_set_option(bufnr, ...)
-    end
-
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
     local opts = { noremap = true, silent = true }
