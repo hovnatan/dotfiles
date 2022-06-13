33c65c1fde4373b0a30f5cef994cf041f2d5950c
Hovnatan Karapetyan <hovnatan@mbp>
2022-06-13 10:58:06 +0400

    tmux autorename

---
+++ .config/nvim/lua/modules/config/lsp/get_opencv_stubs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sSL https://raw.githubusercontent.com/bschnurr/python-type-stubs/add-opencv/cv2/__init__.pyi -o ~/work/stubs/cv2.pyi
--- .config/nvim/lua/modules/config/lsp/init.lua
+++ .config/nvim/lua/modules/config/lsp/init.lua
@@ -1,92 +1,99 @@
-return function() 
+return function()
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    { virtual_text = false }
+  )
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics,
-  { virtual_text = false }
-)
+  local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+      vim.api.nvim_buf_set_option(bufnr, ...)
+    end
 
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "<space>e", '<cmd>lua vim.diagnostic.open_float(0, {scope="line"})<CR>', opts)
+    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap("n", "<space>z", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
   end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
 
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<space>e", '<cmd>lua vim.diagnostic.open_float(0, {scope="line"})<CR>', opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<space>z", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-end
-local lspconfig = require("lspconfig")
-lspconfig.efm.setup({
-  filetypes = { "lua", "javascript", "python" },
-  on_attach = on_attach,
-  init_options = { documentFormatting = true },
-  settings = {
-    rootMarkers = { ".git/" },
-    languages = {
-      lua = { { formatCommand = "stylua --search-parent-directories -", formatStdin = true } },
-      python = {
-        {
-          formatCommand = "isort --stdout --profile black -",
-          formatStdin = true,
+  local lspconfig = require("lspconfig")
+  lspconfig.efm.setup({
+    filetypes = { "lua", "javascript", "python" },
+    on_attach = on_attach,
+    init_options = { documentFormatting = true },
+    settings = {
+      rootMarkers = { ".git/" },
+      languages = {
+        lua = { { formatCommand = "stylua --search-parent-directories -", formatStdin = true } },
+        python = {
+          {
+            formatCommand = "isort --stdout --profile black -",
+            formatStdin = true,
+          },
+          { formatCommand = "black --fast -", formatStdin = true },
         },
-        { formatCommand = "black --fast -", formatStdin = true },
+        javascript = { { formatCommand = "prettier", formatStdin = true } },
       },
-      javascript = { { formatCommand = "prettier", formatStdin = true } },
     },
-  },
-})
+  })
 
-lspconfig.clangd.setup({
-  on_attach = on_attach,
-  flags = { debounce_text_changes = 150 },
-  init_options = { clangdFileStatus = true },
-  cmd = {
-    "/opt/homebrew/opt/llvm/bin/clangd",
-    "--compile-commands-dir=.",
-    "--background-index",
-    "--clang-tidy",
-    "--completion-parse=always",
-    "--completion-style=detailed",
-    "--function-arg-placeholders",
-    "--header-insertion-decorators",
-    "--header-insertion=never",
-    "--cross-file-rename",
-    "--limit-results=0",
-    "-j=4",
-    "--pch-storage=memory",
-  },
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-})
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  flags = { debounce_text_changes = 150 },
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-})
-lspconfig.jsonls.setup({ on_attach = on_attach })
-
+  lspconfig.clangd.setup({
+    on_attach = on_attach,
+    flags = { debounce_text_changes = 150 },
+    init_options = { clangdFileStatus = true },
+    cmd = {
+      "/opt/homebrew/opt/llvm/bin/clangd",
+      "--compile-commands-dir=.",
+      "--background-index",
+      "--clang-tidy",
+      "--completion-parse=always",
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "--header-insertion-decorators",
+      "--header-insertion=never",
+      "--cross-file-rename",
+      "--limit-results=0",
+      "-j=4",
+      "--pch-storage=memory",
+    },
+    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  })
+  lspconfig.pyright.setup({
+    on_attach = on_attach,
+    flags = { debounce_text_changes = 150 },
+    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          stubPath = os.getenv("HOME") .. "/work/stubs",
+        },
+      },
+    },
+  })
+  lspconfig.jsonls.setup({ on_attach = on_attach })
 end
