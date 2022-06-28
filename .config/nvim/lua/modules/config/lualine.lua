3cc6ab31cfd2fbe6502968778b966dfd34286676
Hovnatan Karapetyan <hovnatan@dellxps>
2022-06-28 16:40:45 +0400

    use lualine

--- .config/nvim/lua/modules/config/lualine.lua
+++ .config/nvim/lua/modules/config/lualine.lua
@@ -1,10 +1,10 @@
 return function()
   require("lualine").setup({
     options = {
-      icons_enabled = true,
+      icons_enabled = false,
       theme = "auto",
       component_separators = { left = "|", right = "|" },
-      section_separators = { left = "-", right = "-" },
+      section_separators = { left = "", right = "" },
       disabled_filetypes = {},
       always_divide_middle = true,
       globalstatus = false,
