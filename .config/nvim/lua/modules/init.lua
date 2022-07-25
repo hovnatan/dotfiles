d87aa95ad2e095b4c2dabe039d6147ce878b15d5
Hovnatan Karapetyan <hovnatan@thinkpad>
2022-07-25 19:50:13 +0400

    init

--- .config/nvim/lua/modules/init.lua
+++ .config/nvim/lua/modules/init.lua
@@ -138,6 +138,20 @@ local plugins = {
     },
     config = conf("neoclip"),
   },
+  {
+    "klen/nvim-config-local",
+    config = function()
+      require("config-local").setup({
+        -- Default configuration (optional)
+        config_files = { ".vimrc.lua", ".vimrc" }, -- Config file patterns to load (lua supported)
+        hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
+        autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+        commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
+        silent = false, -- Disable plugin messages (Config loaded/ignored)
+        lookup_parents = true, -- Lookup config files in parent directories
+      })
+    end,
+  }
 }
 
 -- vim.api.nvim_set_keymap("n", "$", "<cmd>lua require'hop'.hint_words()<cr>", {})
