074940a94b5ea831929f59a56f98c5782ad49408
Hovnatan Karapetyan <hovnatan@mbp>
2022-08-03 13:41:40 +0400

    node16

--- .config/nvim/lua/modules/init.lua
+++ .config/nvim/lua/modules/init.lua
@@ -124,7 +124,7 @@ local plugins = {
         hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
         autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
         commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
-        silent = false, -- Disable plugin messages (Config loaded/ignored)
+        silent = true, -- Disable plugin messages (Config loaded/ignored)
         lookup_parents = true, -- Lookup config files in parent directories
       })
     end,
