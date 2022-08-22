60ca6eb32c01468d2eef3bc35f6938ebdf20bdbe
Hovnatan Karapetyan <hovnatan@thinkpad>
2022-08-22 13:44:57 +0400

    init

--- bin/tmux_attach.sh
+++ bin/tmux_attach.sh
@@ -6,8 +6,11 @@ if [ -z $1 ]; then
 fi
 
 if ! tmux has-session -t "$1" 2>/dev/null; then
-  tmux new-session -d -s "$1" -n nvim
+  tmux new-session -d -s "$1" -n nvim1
   tmux new-window -n cmd1 -t "$1"
-  tmux new-window -n r1 -t "$1"
+  tmux new-window -n nvim2 -t "$1"
+  tmux new-window -n cmd2 -t "$1"
+  tmux new-window -n nvim3 -t "$1"
+  tmux new-window -n cmd3 -t "$1"
 fi
 tmux attach-session -t "$1"
