f89ae41fc914b7da94feb1a5be0357f84e6fbad2
Hovnatan Karapetyan <hovnatan@hk-delll>
2022-09-14 11:50:13 +0400

    Deploy locally

--- setup.sh
+++ setup.sh
@@ -117,4 +117,5 @@ ln -sf ~/.dotfiles/.config/wezterm ~/.config/
 ln -sf ~/.dotfiles/.config/sioyek ~/.config/
 
 ln -sf ~/.dotfiles/.local/share/applications/zotero.desktop ~/.local/share/applications/
+ln -sf ~/.dotfiles/.local/share/applications/sioyek.desktop ~/.local/share/applications/
 update-desktop-database ~/.local/share/applications/
