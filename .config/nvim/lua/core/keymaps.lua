1cf1832db8a3abee72394d6f9890860917814deb
Hovnatan Karapetyan <hovnatan@hk-delll>
2022-10-07 11:44:51 +0400

    Add servers table

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -26,52 +26,15 @@ vim.api.nvim_set_keymap("n", "<C-w>k", ":wincmd l<CR>", { noremap = true, silent
 vim.api.nvim_set_keymap("n", "<C-w>h", ":wincmd j<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-w>l", ":wincmd k<CR>", { noremap = true, silent = true })
 
-vim.cmd([[
-let s:savedCpo = &cpo
-set cpo&vim
-
-function! s:VStarsearch_searchCWord()
-	let wordStr = expand("<cword>")
-	if strlen(wordStr) == 0
-		echohl ErrorMsg
-		echo 'E348: No string under cursor'
-		echohl NONE
-		return
-	endif
-
-	if wordStr[0] =~ '\<'
-		let @/ = '\<' . wordStr . '\>'
-	else
-		let @/ = wordStr
-	endif
-
-	let savedUnnamed = @"
-	let savedS = @s
-	normal! "syiw
-	if wordStr != @s
-		normal! w
-	endif
-	let @s = savedS
-	let @" = savedUnnamed
-endfunction
-
-" https://github.com/bronson/vim-visual-star-search/
-function! s:VStarsearch_searchVWord()
-	let savedUnnamed = @"
-	let savedS = @s
-	normal! gv"sy
-	let @/ = '\V' . substitute(escape(@s, '\'), '\n', '\\n', 'g')
-	let @s = savedS
-	let @" = savedUnnamed
-endfunction
-
-nnoremap <silent> * :call <SID>VStarsearch_searchCWord()<CR>:set hls<CR>
-vnoremap <silent> * :<C-u>call <SID>VStarsearch_searchVWord()<CR>:set hls<CR>
-nmap <silent> # *
-vmap <silent> # *
-
-noremap <expr> n 'Nn'[v:searchforward]
-noremap <expr> N 'nN'[v:searchforward]
-
-let &cpo = s:savedCpo
-]])
+vim.api.nvim_set_keymap("n", "*", "", {
+  callback = function()
+    vim.fn.execute("normal! *N")
+    vim.fn.execute("set hlsearch")
+  end,
+})
+vim.api.nvim_set_keymap("n", "#", "", {
+  callback = function()
+    vim.fn.execute("normal! #N")
+    vim.fn.execute("set hlsearch")
+  end,
+})
