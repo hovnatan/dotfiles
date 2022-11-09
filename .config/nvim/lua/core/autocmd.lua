e59bcda3c1d05e0499e947a17b4c05b43d95cf12
Hovnatan Karapetyan <hovnatan@hk-delll>
2022-11-09 14:50:57 +0400

    mac linux

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = "highlight_yank",
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ timeout = 150 })
+    vim.highlight.on_yank({ timeout = 500 })
   end,
 })
 
