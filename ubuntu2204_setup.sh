2b487bc0071029064cdf45ac876f8916633537b5
Hovnatan Karapetyan <hovnatan@hk-delll>
2022-12-12 11:32:36 +0400

    tmux

--- ubuntu2204_setup.sh
+++ ubuntu2204_setup.sh
@@ -72,7 +72,7 @@ export CXXFLAGS="-march=native"
 cd ~/Downloads
 git clone https://github.com/neovim/neovim.git
 cd neovim
-git checkout v0.8.1
+git checkout stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 cd ..
