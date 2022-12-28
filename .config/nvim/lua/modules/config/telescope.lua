6f5df62880c968a083a06690464f3c31cbf45ec2
Hovnatan Karapetyan <hovnatan@hk-delll>
2022-12-28 15:57:37 +0400

    Add .gcloudignore

--- .config/nvim/lua/modules/config/telescope.lua
+++ .config/nvim/lua/modules/config/telescope.lua
@@ -79,6 +79,6 @@ return function()
   end, { noremap = true, silent = true })
   vim.keymap.set("n", "<space>A", telescope_builtin.diagnostics, { noremap = true, silent = true })
   vim.keymap.set("n", "<space>s", telescope_builtin.treesitter, { noremap = true, silent = true })
-  vim.keymap.set("n", "<space>p", telescope_builtin.pickers, { noremap = true, silent = true })
-  vim.keymap.set("n", "<space>[", telescope_builtin.resume, { noremap = true, silent = true })
+  vim.keymap.set("n", "<space>p", telescope_builtin.resume, { noremap = true, silent = true })
+  vim.keymap.set("n", "<space>[", telescope_builtin.pickers, { noremap = true, silent = true })
 end
