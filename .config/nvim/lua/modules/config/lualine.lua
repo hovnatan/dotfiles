54bb1fd42f3bcbca9640f945a2dff36064114396
Hovnatan Karapetyan <>
2023-01-12 10:55:32 +0400

    Exit on sigchld

--- .config/nvim/lua/modules/config/lualine.lua
+++ .config/nvim/lua/modules/config/lualine.lua
@@ -12,7 +12,7 @@ return function()
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { { "filename", file_status = true, path = 1 }, "searchcount" },
+      lualine_c = { { "filename", file_status = true, path = 1 } },
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
