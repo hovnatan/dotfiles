a8634f43a9bc3f2345e207bfb643c2aebcd72f8d
Hovnatan Karapetyan <hovnatan@hk-delll>
2023-01-19 16:18:34 +0400

    s

--- sioyek.sh
+++ sioyek.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-firejail --noprofile --net=none ~/Downloads/Sioyek-x86_64.AppImage "$1"
+if [ $# -eq 0 ]; then
+  firejail --noprofile --net=none -- ~/Downloads/Sioyek-x86_64.AppImage
+else
+  firejail --noprofile --net=none -- ~/Downloads/Sioyek-x86_64.AppImage "$1"
+fi
+
 
 
