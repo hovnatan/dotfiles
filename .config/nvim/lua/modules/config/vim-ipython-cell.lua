184535ee78f848eb35c3bfeac2c7975ae17fb905
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-02-08 13:55:09 +0400

    lazynvim

--- .config/nvim/lua/modules/config/vim-ipython-cell.lua
+++ .config/nvim/lua/modules/config/vim-ipython-cell.lua
@@ -2,19 +2,17 @@ vim.g.ipython_cell_delimit_cells_by = "tags"
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function()
-    vim.api.nvim_buf_set_keymap(
-      0,
+    vim.keymap.set(
       "n",
       "<C-c><C-c>",
       ":IPythonCellExecuteCellVerbose<CR>",
-      { noremap = true, silent = true }
+      { noremap = true, silent = true, buffer = true }
     )
-    vim.api.nvim_buf_set_keymap(
-      0,
+    vim.keymap.set(
       "n",
       "<C-c><C-n>",
       ":IPythonCellExecuteCellVerboseJump<CR>",
-      { noremap = true, silent = true }
+      { noremap = true, silent = true, buffer = true }
     )
   end,
 })
