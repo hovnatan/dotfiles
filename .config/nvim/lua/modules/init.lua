1ff46d2cf012a23dee9daf951667511add0635f5
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-02-20 10:01:38 +0400

    remove ufo

--- .config/nvim/lua/modules/init.lua
+++ .config/nvim/lua/modules/init.lua
@@ -209,17 +209,6 @@ require("lazy").setup({
     end,
   },
   {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    config = function()
-      require("ufo").setup({
-        provider_selector = function(bufnr, filetype, buftype)
-          return { "treesitter", "indent" }
-        end,
-      })
-    end,
-  },
-  {
     "kylechui/nvim-surround",
     config = function()
       require("nvim-surround").setup({
