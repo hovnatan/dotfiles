dd8924493647d19301901c6d056283822241cc4c
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-03-14 15:22:06 +0400

    Small code cleanup

--- bin/tmux_attach.sh
+++ bin/tmux_attach.sh
@@ -8,5 +8,6 @@ fi
 if ! tmux has-session -t "$1" 2>/dev/null; then
   tmux new-session -d -s "$1" -n nvim
   tmux new-window -n cmd -t "$1"
+  tmux new-window -n todo -t "$1"
 fi
 tmux attach-session -t "$1"
