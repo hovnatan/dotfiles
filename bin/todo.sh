6c39c1cd3d4fb8a594990ed8fafea630d1cdeb84
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-03-21 09:29:55 +0400

    N

--- bin/todo.sh
+++ bin/todo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -eq 0 ]; then
-  nvim "$HOME/Dropbox/container_in_out/todo/todo.md"
+  nvim "$HOME/Dropbox/container_in_out/todo/todo.txt"
 else
-  nvim "$HOME/Dropbox/container_in_out/todo/todo_$1.md"
+  nvim "$HOME/Dropbox/container_in_out/todo/todo_$1.txt"
 fi
