37d17ada2f418cdce48e184dbb3c8abbd2b0edf4
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-05-18 10:44:43 +0400

    a

--- .config/nvim/lua/modules/config/lualine.lua
+++ .config/nvim/lua/modules/config/lualine.lua
@@ -18,7 +18,7 @@ require("lualine").setup({
         hide_filename_extension = false,
         show_modified_status = true,
         mode = 0,
-        max_length = vim.o.columns * 2 / 3,
+        max_length = vim.o.columns * 3 / 4,
         filetype_names = {
           TelescopePrompt = "Telescope",
         },
