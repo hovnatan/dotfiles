1a119c2ed82d797f6cf761e8c7e7e0e7f3359b1f
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-06-06 10:32:06 +0400

    Minor change

--- .config/nvim/lua/modules/config/lualine.lua
+++ .config/nvim/lua/modules/config/lualine.lua
@@ -10,26 +10,9 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_c = {},
-    lualine_b = {
-      {
-        "buffers",
-        show_filename_only = true,
-        hide_filename_extension = false,
-        show_modified_status = true,
-        mode = 0,
-        max_length = vim.o.columns * 3 / 4,
-        filetype_names = {
-          TelescopePrompt = "Telescope",
-        },
-        use_mode_colors = true,
-        symbols = {
-          modified = "+",
-          alternate_file = "#",
-        },
-      },
-    },
-    lualine_x = { "branch", "diff", "diagnostics", "encoding", "fileformat", "filetype" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { { "filename", file_status = true, path = 1 } },
+    lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
   },
