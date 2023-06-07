bd9de6a3e443c04821c6a861796591419662db7c
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-06-07 08:58:06 +0400

    u

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -57,48 +57,29 @@ function __zoxide_zi_c
 end
 bind -M insert \ec  __zoxide_zi_c
 
-abbr ll  "ls -aslh"
 abbr rsync  "rsync -a --info=progress2"
+abbr ll  "ls -aslh"
 abbr n   "nvim"
-abbr nn  "nvim -u NONE"
-abbr np  "nvim --noplugin"
 abbr g   "grep"
-
-function ranger_fm
-  if not set -q RANGER_LEVEL
-    ranger --choosedir="$HOME/.rangerdir"; cd (cat $HOME/.rangerdir)
-  else
-    exit
-  end
-end
-
 abbr da  "docker exec -it (docker ps | head -n 2 | tail -n 1 | awk '{print \$1}') /bin/bash"
 abbr ta  "tmux_attach.sh"
 abbr tl  "tmux list-sessions"
-abbr tc "tmux capture-pane -pJ -S - | nvim -R '+set ft=log|set nowrap|set foldlevel=99|DisableWhitespace' '+norm G' --"
-abbr jc "journalctl -b --no-pager | nvim -R '+set ft=log|set nowrap|set foldlevel=99|DisableWhitespace' '+norm G' --"
+abbr tc  "tmux capture-pane -pJ -S - | nvim -R '+set ft=log|set nowrap|set foldlevel=99|DisableWhitespace' '+norm G' --"
+abbr jc  "journalctl -b --no-pager | nvim -R '+set ft=log|set nowrap|set foldlevel=99|DisableWhitespace' '+norm G' --"
 abbr prg "pdfgrep --cache -ri --page-range 1"
 abbr rgh "rg --hidden --no-ignore-vcs"
-abbr mhq "mpv --profile=hq"
-abbr mhq2 "mpv --profile=hq2"
-abbr mhq3 "mpv --profile=hq3"
-abbr mnr "mpv --no-resume-playback"
-abbr mhqnr "mpv --no-resume-playback --profile=hq"
-abbr ync "yay --noconfirm"
-abbr bc "bc -l"
-abbr rf "rm -rfvI"
-abbr b "batcat"
-abbr h "htop"
-abbr hh "htop -u $USER"
-abbr p "python"
-abbr pd "python3 -m ipdb -c continue"
-abbr pu "pipdeptree --warn silence | grep -E '^[a-zA-Z]+' | awk -F== '{print\$1}' | xargs pip3 install -U"
-abbr hm "history merge"
+abbr bc  "bc -l"
+abbr rf  "rm -rfvI"
+abbr h   "htop"
+abbr hh  "htop -u $USER"
+abbr p   "python"
+abbr pd  "python3 -m ipdb -c continue"
+abbr pu  "pipdeptree --warn silence | grep -E '^[a-zA-Z]+' | awk -F== '{print\$1}' | xargs pip3 install -U"
+abbr hm  "history merge"
 abbr rfc "source ~/.config/fish/config.fish"
 abbr ... "../.."
-abbr .... "../../.."
-abbr sv "source .venv/bin/activate.fish"
-abbr xt "TERM=xterm-256color"
+abbr sv  "source .venv/bin/activate.fish"
+abbr xt  "TERM=xterm-256color"
 
 set -u VIRTUAL_ENV_DISABLE_PROMPT 1
 set -u fish_command_timer_enabled 1
