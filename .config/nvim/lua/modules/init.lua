c12ea8ecd14972ac6fe06b631f47a780a42e5bf7
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-10-03 09:12:28 -0700

    atuin

--- .config/nvim/lua/modules/init.lua
+++ .config/nvim/lua/modules/init.lua
@@ -13,6 +13,39 @@ vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+  },
+  {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
