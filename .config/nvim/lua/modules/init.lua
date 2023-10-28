2363563d970ab37fd4a906121f0a8ac1f751197e
Hovnatan Karapetyan <hovnatan@gmail.com>
2023-10-28 11:40:45 -0700

    Update readme

--- .config/nvim/lua/modules/init.lua
+++ .config/nvim/lua/modules/init.lua
@@ -29,6 +29,12 @@ require("lazy").setup({
     },
     config = function()
       require("noice").setup({
+        cmdline = {
+          enabled = true,
+          format = {
+            conceal = false,
+          },
+        },
         lsp = {
           signature = {
             enabled = true,
@@ -48,6 +54,18 @@ require("lazy").setup({
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
           lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
+        },
+        views = {
         },
       })
       local telescope = require("telescope")
