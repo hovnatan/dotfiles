be3db82fc0920c92626c407b60d01ebd1b84c996
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-04-08 00:40:37 +0400

    a

--- small_utils/convert_to_pdf.sh
+++ small_utils/convert_to_pdf.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-sudo apt install imagemagick poppler-utils ocrmypdf
+# sudo apt install imagemagick poppler-utils ocrmypdf
 
-mkdir -p out_pdf
+# sudo vim /etc/ImageMagick-6/policy.xml # remove lines disabling PDF  also maybe increase memory and disk resources size
+
+input_folder="."
+
+out_folder="out_pdf"
+
+mkdir -p "$out_folder"
 
 N=8 # num parallel tasks
 
 (
-for f in *.tif; do 
+for f in "$input_folder"/*.tif; do 
   ((i=i%N)); ((i++==0)) && wait
 
   filename=$(basename -- "$f")
   extension="${filename##*.}"
   filename="${filename%.*}"
   echo "$filename"
-  convert "$f" "out_pdf/${filename}.pdf"
+  convert "$f" "$out_folder/${filename}.pdf"
 done
 )
 
-
-pdfunite out_pdf/*.pdf ./out.pdf
+pdfunite "$out_folder/"*.pdf ./out.pdf
 ocrmypdf ./out.pdf ./out_ocr.pdf
