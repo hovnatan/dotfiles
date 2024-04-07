627bdbaecb7b0fa810cda24c7c90c448dfe3c773
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-04-07 23:43:54 +0400

    a

--- small_utils/convert_to_pdf.sh
+++ small_utils/convert_to_pdf.sh
@@ -4,13 +4,19 @@ sudo apt install imagemagick poppler-utils ocrmypdf
 
 mkdir -p out_pdf
 
+N=8 # num parallel tasks
+
+(
 for f in *.tif; do 
+  ((i=i%N)); ((i++==0)) && wait
+
   filename=$(basename -- "$f")
   extension="${filename##*.}"
   filename="${filename%.*}"
   echo "$filename"
   convert "$f" "out_pdf/${filename}.pdf"
 done
+)
 
 
 pdfunite out_pdf/*.pdf ./out.pdf
