cf19f13bbb4763076bdc2a01e0511aec391b6e5d
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-04-25 23:56:09 +0400

    a

--- bin/cw.sh
+++ bin/cw.sh
@@ -40,11 +40,11 @@ echo -n "$COLOR" > ~/.my_colors
 echo -n "set -U _reload_color_config ""$COLOR" | fish
 
 # kitty @ --to $HOME/.tmpkitty set-colors -a -c ~/.config/kitty/base16-gruvbox-dark-medium.conf
-"$script_full_path/color_switcher.sh" ~/.config/alacritty/alacritty.toml ~/.config/alacritty/option ~/.config/alacritty/colors/$COLOR
+"$script_full_path/color_switcher.sh" ~/.dotfiles/config/alacritty/alacritty.toml ~/.dotfiles/.config/alacritty/option ~/.dotfiles/.config/alacritty/colors/$COLOR
 
 if [[ "$WSL_DISTRO_NAME" ]]; then
   WINDOWS_HOME=$(wslpath "$(wslvar USERPROFILE)")
-  cp ~/.config/alacritty/alacritty.yml "$WINDOWS_HOME/AppData/Roaming/alacritty/alacritty.yml"
+  cp ~/.dotfiles/.config/alacritty/alacritty.yml "$WINDOWS_HOME/AppData/Roaming/alacritty/alacritty.yml"
 fi
 
 tmux source ~/.config/tmux/tmux.conf
