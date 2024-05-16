b72b9377016ea547d39c13b94ec8d82a4c4eae33
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-05-16 21:23:21 +0400

    a

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -17,7 +17,6 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 5
-vim.opt.termguicolors = true
 vim.opt.smartindent = true
 vim.opt.showmatch = true
 vim.opt.ignorecase = true
--- .config/nvim/lua/modules/config/lsp/get_opencv_stubs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -sSL https://raw.githubusercontent.com/bschnurr/python-type-stubs/add-opencv/cv2/__init__.pyi -o ~/work/stubs/cv2.pyi
