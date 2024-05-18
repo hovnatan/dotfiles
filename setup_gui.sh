9094019de50809ef75b6b32371207d7f1bb9391a
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-05-18 08:48:56 +0400

    a

--- setup_gui.sh
+++ setup_gui.sh
@@ -5,4 +5,4 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'cycle-window
 sudo apt install -y default-jre libreoffice-java-common firejail
 
 # for sioyek
-sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev libgl-dev libxi-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qt3d5-dev qtdeclarative5-dev libharfbuzz-dev
+sudo apt-get install -y libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev libgl-dev libxi-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qt3d5-dev qtdeclarative5-dev libharfbuzz-dev
