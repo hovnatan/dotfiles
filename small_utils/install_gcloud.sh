b35a26ea8655b60c3fe56d7b1726c668d5ed1e1f
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-05-20 13:09:30 +0400

    a

--- small_utils/install_gcloud.sh
+++ small_utils/install_gcloud.sh
@@ -1,1 +1,6 @@
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - && apt-get update -y && apt-get install google-cloud-cli -y
+#!/usr/bin/env bash
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+sudo apt-get update -y
+sudo apt-get install google-cloud-cli -y
