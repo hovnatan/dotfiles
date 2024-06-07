d7288132ec9532956cca6b8acfa30d090aa967be
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-06-07 08:40:36 +0000

    Codespell fix

--- ubuntu2404_setup_user.sh
+++ ubuntu2404_setup_user.sh
@@ -42,16 +42,16 @@ cd ~/Downloads
 rm -rf neovim
 git clone --depth 1 --branch stable https://github.com/neovim/neovim.git 
 cd neovim
-make CMAKE_BUILD_TYPE=Release
-sudo make install
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local"
+make install
 cd ..
 
 rm -rf tmux
 git clone --depth 1 --branch 3.4 https://github.com/tmux/tmux.git
 cd tmux
 sh autogen.sh
-./configure && make
-sudo make install
+./configure --prefix=$HOME/.local && make
+make install
 cd ..
 
 rm -rf fish-shell
@@ -59,7 +59,7 @@ git clone --depth 1 --branch 3.7.1 https://github.com/fish-shell/fish-shell.git
 cd fish-shell
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 make
 sudo make install
 cd ../..
@@ -67,7 +67,7 @@ cd ../..
 rm -rf alacritty
 git clone --depth 1 --branch v0.13.2 https://github.com/alacritty/alacritty.git
 cd alacritty
-sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+tic -xe alacritty,alacritty-direct extra/alacritty.info
 # cargo build --release --no-default-features --features=wayland
 # sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
 # sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
