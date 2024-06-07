bc905b46f868670c010b78b7d65f17e8d32318f5
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-06-07 12:24:30 +0400

    Codespell fix

--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,11 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 NAME=hk_dev_env
 
 mkdir -p /home/$USER/Dropbox/${NAME}_container_in_out
 mkdir -p /home/$USER/${NAME}_container_in_out
 
-docker rm hk_dev_env
+docker stop $NAME
+docker rm $NAME
 
-docker run --hostname $NAME --volume=/etc/localtime:/etc/localtime:ro --volume=/home/$USER/${NAME}_container_in_out:/home/$USER --volume=/home/$USER/Dropbox/${NAME}_container_in_out:/home/$USER/Dropbox/${NAME}_container_in_out -p 8888:8888 -ti --name=$NAME $NAME
+docker run --hostname $NAME --volume=/home/$USER/${NAME}_container_in_out:/home/$USER --volume=/home/$USER/Dropbox/${NAME}_container_in_out:/home/$USER/Dropbox/${NAME}_container_in_out -p 8888:8888 -ti --name=$NAME $NAME
