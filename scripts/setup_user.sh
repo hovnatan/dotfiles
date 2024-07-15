25da499afc4e15fa82b46187b1400909daa7104b
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-07-15 12:14:50 -0400

    a

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -11,13 +11,3 @@ mkdir -p ~/.vim/undodir
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.tmux.conf -o ~/.tmux.conf
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.zshrc -o ~/.zshrc
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.vimrc -o ~/.vimrc
-curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.profile_local -o ~/.profile_local
-
-if ! grep -q ".profile_local" "$HOME/.profile"; then
-    cat <<EOT >> $HOME/.profile
-
-if [[ -f "\$HOME/.profile_local" ]]; then
-    source "\$HOME/.profile_local"
-fi
-EOT
-fi
