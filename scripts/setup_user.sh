55175e345756a78d1a7df744fc449563b1a36d16
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-07-15 11:27:52 -0400

    a

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -10,16 +10,9 @@ curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.tmux.conf -o ~/.t
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.zshrc -o ~/.zshrc
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.vimrc -o ~/.vimrc
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.profile_local -o ~/.profile_local
-curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.bashrc_local -o ~/.bashrc_local
-
-cat <<EOT >> ~/.bashrc
-if [[ -f "$HOME/.bashrc_local" ]]; then
-    source "$HOME/.bashrc_local"
-fi
-EOT
 
 cat <<EOT >> ~/.profile
-if [[ -f "$HOME/.profile_local" ]]; then
-    source "$HOME/.profile_local"
+if [[ -f "\$HOME/.profile_local" ]]; then
+    source "\$HOME/.profile_local"
 fi
 EOT
