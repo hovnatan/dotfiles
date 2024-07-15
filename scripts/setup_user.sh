45bd0aebf4d1acf26d5619483544d362564da8a6
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-07-15 11:38:28 -0400

    a

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/scripts/setup_user.sh -o ~/setup_user.sh
+
 set -e
 
 cd ~
@@ -11,8 +13,11 @@ curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.zshrc -o ~/.zshrc
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.vimrc -o ~/.vimrc
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.profile_local -o ~/.profile_local
 
-cat <<EOT >> ~/.profile
-if [[ -f "\$HOME/.profile_local" ]]; then
-    source "\$HOME/.profile_local"
-fi
+if ! grep -q ".profile_local" "$HOME/.profile"; then
+    cat <<EOT >> $HOME/.profile
+
+    if [[ -f "\$HOME/.profile_local" ]]; then
+        source "\$HOME/.profile_local"
+    fi
 EOT
+fi
