b63af2ce2e2c89b7b326bf7f4894bf3549d81fb7
Hovnatan Karapetyan <>
2024-08-09 13:45:08 +0400

    a

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -11,13 +11,17 @@ mkdir -p ~/.vim/undodir
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.tmux.conf -o ~/.tmux.conf
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.zshrc -o ~/.zshrc
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.vimrc -o ~/.vimrc
-curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.gitconfig -o ~/.gitconfig
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.gitconfig_common -o ~/.gitconfig_common
 curl https://raw.githubusercontent.com/hovnatan/dotfiles/main/.bashrc_local -o ~/.bashrc_local
 cat <<EOT >> ~/.bashrc
 if [[ -f "$HOME/.bashrc_local" ]]; then
     source "$HOME/.bashrc_local"
 fi
+EOT
+
+cat <<EOT >> ~/.gitconfig
+[include]
+  path = ~/.gitconfig_common
 EOT
 
 mkdir -p ~/.ssh
