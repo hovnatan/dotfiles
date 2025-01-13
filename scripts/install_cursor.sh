03ea20dbd8c14b1c6f073af9d3ddc417d150fbe3
Hovnatan Karapetyan <hovnatan@gmail.com>
2025-01-13 18:02:10 +0400

    a

--- scripts/install_cursor.sh
+++ scripts/install_cursor.sh
@@ -2,13 +2,20 @@
 
 set -euo pipefail
 
-mkdir -p ~/opt/bin
-mkdir -p ~/opt/share
+# mkdir -p ~/opt/share
+# wget -O ~/opt/share/cursor.svg "https://mintlify.s3-us-west-1.amazonaws.com/cursor/images/logo/app-logo.svg"
 
-wget -O ~/opt/share/cursor.svg "https://mintlify.s3-us-west-1.amazonaws.com/cursor/images/logo/app-logo.svg"
+# mkdir -p ~/opt/bin
+# wget -O ~/opt/bin/cursor.AppImage "https://downloader.cursor.sh/linux/appImage/x64"
+# chmod +x ~/opt/bin/cursor.AppImage
 
-wget -O ~/opt/bin/cursor.AppImage "https://downloader.cursor.sh/linux/appImage/x64"
-chmod +x ~/opt/bin/cursor.AppImage
-
-ln -sf ~/.dotfiles/.local/share/applications/cursor.desktop ~/.local/share/applications
+mkdir -p ~/.local/share/applications
+cat << 'EOL' > ~/.local/share/applications/cursor.desktop
+[Desktop Entry]
+Name=Cursor
+Exec=/home/hovnatan/opt/bin/cursor.AppImage --no-sandbox
+Icon=/home/hovnatan/opt/share/cursor.svg
+Type=Application
+Categories=Development;
+EOL
 update-desktop-database ~/.local/share/applications
