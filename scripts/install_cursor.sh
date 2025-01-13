272b39458bdd013ea1f4c731a4061e560364172a
Hovnatan Karapetyan <hovnatan@gmail.com>
2025-01-13 18:02:32 +0400

    a

--- scripts/install_cursor.sh
+++ scripts/install_cursor.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-# mkdir -p ~/opt/share
-# wget -O ~/opt/share/cursor.svg "https://mintlify.s3-us-west-1.amazonaws.com/cursor/images/logo/app-logo.svg"
+mkdir -p ~/opt/share
+wget -O ~/opt/share/cursor.svg "https://mintlify.s3-us-west-1.amazonaws.com/cursor/images/logo/app-logo.svg"
 
-# mkdir -p ~/opt/bin
-# wget -O ~/opt/bin/cursor.AppImage "https://downloader.cursor.sh/linux/appImage/x64"
-# chmod +x ~/opt/bin/cursor.AppImage
+mkdir -p ~/opt/bin
+wget -O ~/opt/bin/cursor.AppImage "https://downloader.cursor.sh/linux/appImage/x64"
+chmod +x ~/opt/bin/cursor.AppImage
 
 mkdir -p ~/.local/share/applications
 cat << 'EOL' > ~/.local/share/applications/cursor.desktop
