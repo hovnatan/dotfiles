2810ea2bc191b1da0ca6f3d623a7f53f7f74c972
Hovnatan Karapetyan <hovnatan@gmail.com>
2025-08-03 09:44:22 +0400

    Update docker_run.sh to improve path handling and directory creation
    - Correct the HOME variable to include a leading slash for proper path resolution.
    - Introduce DROPBOX_PATH variable for better clarity and maintainability.
    - Update directory creation commands to use the new DROPBOX_PATH variable for consistency.

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,13 +2,13 @@
 
 NAME=hk_dev_env
 
-HOME=Users
+HOME=/Users
+DROPBOX_PATH=$HOME/$USER/Library/CloudStorage/Dropbox
 
-mkdir -p /$HOME/$USER/Dropbox/${NAME}_container_in_out
-mkdir -p /$HOME/$USER/${NAME}_container_in_out
+mkdir -p $DROPBOX_PATH/${NAME}_container_in_out
+mkdir -p $HOME/$USER/${NAME}_container_in_out
 
 docker stop $NAME
 docker rm $NAME
 
-
-docker run --hostname $NAME --volume=/$HOME/$USER/${NAME}_container_in_out:/home/$USER --volume=/$HOME/$USER/Dropbox/${NAME}_container_in_out:/home/$USER/Dropbox/${NAME}_container_in_out -p 8888:8888 -ti --name=$NAME $NAME
+docker run --hostname $NAME --volume=$HOME/$USER/${NAME}_container_in_out:/home/$USER --volume=$DROPBOX_PATH/${NAME}_container_in_out:/home/$USER/Dropbox/${NAME}_container_in_out -p 8888:8888 -ti --name=$NAME $NAME
