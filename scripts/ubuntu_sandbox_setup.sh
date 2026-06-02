7a100535102d6aa43434a5ba70229dec2a66fbef
Hovnatan Karapetyan <hovnatan@gmail.com>
2026-06-02 16:36:17 -0400

    Add Ubuntu sandbox setup script to automate package installation for development environment

---
+++ scripts/ubuntu_sandbox_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get update
+
+sudo apt-get install -y unminimize git curl ca-certificates wget htop vim jq sudo gnupg zsh tmux xz-utils make adduser psmisc git-lfs unzip zstd build-essential squashfs-tools strace openssh-server
